63a2e96b10e1b74fb197ccbeca6ef979b0bc4c81
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-04-23 16:09:11 +0200

    added DietaryGuidance treatment child class

---
+++ BookRight.DomainLib/Entities/Treatments/DietaryGuidance/DietaryGuidance30.cs
@@ -0,0 +1,11 @@
+﻿namespace BookRight.DomainLib.Entities.Treatments.DietaryGuidance;
+
+public class DietaryGuidance30 : Treatment
+{
+    public DietaryGuidance30()
+    {
+        Name = "Dietary Guidance follow-up";
+        Price = 450m;
+        Duration = TimeSpan.FromMinutes(30);
+    }
+}
