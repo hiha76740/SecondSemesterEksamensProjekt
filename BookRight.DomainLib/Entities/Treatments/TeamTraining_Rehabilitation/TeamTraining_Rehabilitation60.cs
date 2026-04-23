cc552865be9e976c6da907a78a1a48c9c0fa6101
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-04-23 16:10:16 +0200

    added TeamTraining_Rehabilitation treatment child class

---
+++ BookRight.DomainLib/Entities/Treatments/TeamTraining_Rehabilitation/TeamTraining_Rehabilitation60.cs
@@ -0,0 +1,14 @@
+﻿namespace BookRight.DomainLib.Entities.Treatments.TeamTraining_Rehabilitation;
+
+public class TeamTraining_Rehabilitation60 : Treatment
+{
+    public int teamSizeLimit { get; private set; }
+
+    public TeamTraining_Rehabilitation60()
+    {
+        Name = "Team Training Rehabilitation";
+        Price = 150;
+        Duration = TimeSpan.FromMinutes(60);
+        teamSizeLimit = 6;
+    }
+}
