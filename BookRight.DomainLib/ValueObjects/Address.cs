8823ce2faa4f50a90749d8d188293781458a20f8
nico003t <nicolaihlb@gmail.com>
2026-04-28 11:51:16 +0200

    Added: Value Objects - Address, Email and PhoneNumber
    Deleted:
    Dummy.cs

---
+++ BookRight.DomainLib/ValueObjects/Address.cs
@@ -0,0 +1,36 @@
+﻿using BookRight.DomainLib.Exceptions;
+
+namespace BookRight.DomainLib.ValueObjects;
+
+public record Address
+{
+    public string Street { get; init; }
+    public string PostalCode { get; init; }
+    public string City { get; init; }
+
+
+    public Address (string street, string postalCode, string city)
+    {
+        if (string.IsNullOrWhiteSpace(street))
+            throw new DomainException("Streetname is required");
+
+        string normalisedStreet = street.Trim();
+
+
+        if (string.IsNullOrWhiteSpace(postalCode))
+            throw new DomainException("Postalcode is required");
+
+        string normalisedPostalCode = postalCode.Trim();
+
+
+        if (string.IsNullOrWhiteSpace(city))
+            throw new DomainException("City is required");
+        
+        string normalisedCity = city.Trim();
+
+
+        Street = normalisedStreet;
+        PostalCode = normalisedPostalCode;
+        City = normalisedCity;
+    }
+}
---
+++ BookRight.DomainLib/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,32 @@
+﻿using BookRight.DomainLib.Exceptions;
+
+namespace BookRight.DomainLib.ValueObjects;
+
+public record PhoneNumber
+{
+    public string Number { get; init; }
+
+
+    public PhoneNumber (string number)
+    {
+        if (string.IsNullOrWhiteSpace(number))
+            throw new DomainException("Phonenumber is required");
+
+        string normalisedNumber = number
+            .Trim()
+            .Replace(" ", "");
+
+        if (normalisedNumber.StartsWith("+45"))
+            normalisedNumber = normalisedNumber[3..];
+
+        if (normalisedNumber.StartsWith("0045"))
+            normalisedNumber = normalisedNumber[4..];
+
+        if (normalisedNumber.Length != 8)
+            throw new DomainException("Phonenumber must contain 8 digits");
+
+
+        Number = normalisedNumber;
+    }
+
+}
