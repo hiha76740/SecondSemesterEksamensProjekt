6b9c6155b549f6886dd5c3a55eb50bd6c71992e4
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-04-29 08:57:34 +0200

    Added Summary to Clinic and OpeningHours Value object

--- BookRight.DomainLib/ValueObjects/OpeningHours.cs
+++ BookRight.DomainLib/ValueObjects/OpeningHours.cs
@@ -2,6 +2,12 @@
 
 namespace BookRight.DomainLib.ValueObjects;
 
+/// <summary>
+/// Represents the opening and closing hours for a single day period.
+/// </summary>
+/// <remarks>The opening and closing times must be on the same day, and the closing time must be after the opening
+/// time. This record is immutable and is typically used to specify business or service availability within a single
+/// calendar day.</remarks>
 public record OpeningHours
 {
     public DateTime Open { get; init; }
