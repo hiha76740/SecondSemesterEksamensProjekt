95437c28d3b323cc62fa38a2e002f474e785cf51
nico003t <nicolaihlb@gmail.com>
2026-04-29 12:54:47 +0200

    Added: Customer constructor and method Create.

--- BookRight.DomainLib/Entities/Customers/Customer.cs
+++ BookRight.DomainLib/Entities/Customers/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using BookRight.DomainLib.ValueObjects;
+using BookRight.DomainLib.Exceptions;
 
 namespace BookRight.DomainLib.Entities.Customers;
 
@@ -12,4 +13,55 @@ public class Customer : AggregateRoot
     public Email Email { get; private set; }
     public PhoneNumber PhoneNumber { get; private set; }
 
+
+    private Customer(
+            string firstName,
+            string lastName,
+            DateTime birthdate,
+            string? note, 
+            Address address,
+            Email email,
+            PhoneNumber phoneNumber)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new DomainException("Customer must have a firstname");
+
+        string normalisedFirstName = firstName.Trim();
+
+
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new DomainException("Customer must have a lastname");
+
+        string normalisedLastName = lastName.Trim();
+
+
+        DateTime normalisedBirthDate = birthdate.Date;
+        if (normalisedBirthDate > DateTime.Today)
+            throw new DomainException("Birthdate cannot be in future");
+        if (normalisedBirthDate < new DateTime(1900, 1, 1))
+            throw new DomainException("Birthdate cannot be this far back");
+
+
+        Id = Guid.NewGuid();
+        FirstName = normalisedFirstName
+        LastName = normalisedLastName;
+        Birthdate = normalisedBirthDate;
+        Note = note;
+        Address = address ?? throw new DomainException("Customer must have an address");
+        Email = email ?? throw new DomainException("Customer must have an email");
+        PhoneNumber = phoneNumber ?? throw new DomainException("Customer must have a phonenumber");
+    }
+
+    public static Customer Create(
+            string firstName,
+            string lastName,
+            DateTime birthdate,
+            string? note,
+            Address address,
+            Email email,
+            PhoneNumber phoneNumber)
+    {
+        var customer = new Customer(firstName, lastName, birthdate, note, address, email, phoneNumber);
+        return customer;
+    }
 }
