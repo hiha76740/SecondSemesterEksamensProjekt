15f9d70e3db1b135b1c0a5615c499057f50f5659
nico003t <nicolaihlb@gmail.com>
2026-04-29 10:15:11 +0200

    Added: Customer with properties according to Class-Diagram v.0.0.1

---
+++ BookRight.DomainLib/Entities/Customers/Customer.cs
@@ -0,0 +1,15 @@
+﻿using BookRight.DomainLib.ValueObjects;
+
+namespace BookRight.DomainLib.Entities.Customers;
+
+public class Customer : AggregateRoot
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public DateTime Birthdate { get; private set; }
+    public string? Note {  get; private set; }
+    public Address Address { get; private set; } 
+    public Email Email { get; private set; }
+    public PhoneNumber PhoneNumber { get; private set; }
+
+}
