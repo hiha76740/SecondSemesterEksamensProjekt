8c68a06fde04edf99cca51e4d6dc655218975c4a
nico003t <nicolaihlb@gmail.com>
2026-04-29 21:01:11 +0200

    Added: Class CustomerTests with testdata.

---
+++ BookRight.DomainLib.Tests/CustomerTests.cs
@@ -0,0 +1,25 @@
+﻿using BookRight.DomainLib.Entities.Customers;
+using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+using System.Net;
+using Xunit;
+
+namespace BookRight.DomainLib.Tests;
+
+public class CustomerTests
+{
+    private static string FirstName => "Poul";
+    private static string LastName => "Pedersen";
+    private static DateTime Birthdate => new DateTime(1965, 6, 18);
+    private static string? Note => "Peanutbutter-allergy";
+    private static Address Address => new Address("Test Avenue 21", "1234", "Testville");
+    private static Email Email => new Email("PoulP@testmail.k");
+    private static PhoneNumber PhoneNumber => new PhoneNumber("87654321");
+
+
+    //Arrange
+
+    //Act
+
+    //Assert
+}
