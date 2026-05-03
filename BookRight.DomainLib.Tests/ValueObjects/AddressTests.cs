25f4305cd879eb0d5e224717f094b3c6e3106338
nico003t <nicolaihlb@gmail.com>
2026-05-04 01:11:37 +0200

    Added: Class AddressTests with testdata and Helpermethod CreateAddressWithValidData()

---
+++ BookRight.DomainLib.Tests/ValueObjects/AddressTests.cs
@@ -0,0 +1,22 @@
+﻿using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+
+namespace BookRight.DomainLib.Tests.ValueObjects;
+
+public class AddressTests
+{
+    private static string Street => "Test Allé";
+    private static string PostalCode => "8000";
+    private static string City => "Aarhus C";
+
+    private static Address CreateAddressWithValidData(
+        string? street = null,
+        string? postalCode = null,
+        string? city = null)
+        => new Address(
+            street ?? Street,
+            postalCode ?? PostalCode,
+            city ?? City);
+
+
+}
