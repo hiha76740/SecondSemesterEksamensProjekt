a4a8c5fba5d0d71d85cd0e8e6055bce7db8282b8
nico003t <nicolaihlb@gmail.com>
2026-05-03 20:37:45 +0200

    Changed: Helpermethod CreateCustomerWithValidData() and testdata has been updated to reflect changes in Customer.

--- BookRight.DomainLib.Tests/CustomerTests.cs
+++ BookRight.DomainLib.Tests/CustomerTests.cs
@@ -8,31 +8,31 @@ namespace BookRight.DomainLib.Tests;
 
 public class CustomerTests
 {
-    private static string FirstName => "Poul";
-    private static string LastName => "Pedersen";
+    private static string Firstname => "Poul";
+    private static string Lastname => "Pedersen";
     private static DateTime Birthdate => new DateTime(1965, 6, 18);
-    private static string? Note => "Peanutbutter-allergy";
+    private static string Note => "Peanutbutter-allergy";
     private static Address Address => new Address("Test Avenue 21", "1234", "Testville");
-    private static Email Email => new Email("PoulP@testmail.k");
+    private static Email Email => new Email("PoulP@testmail.com");
     private static PhoneNumber PhoneNumber => new PhoneNumber("87654321");
 
     private static Customer CreateCustomerWithValidData(
         string? firstName = null,
         string? lastName = null,
         DateTime? birthDate = null,
-        string? note = null,
         Address? address = null,
         Email? email = null,
-        PhoneNumber? phoneNumber = null)
+        PhoneNumber? phoneNumber = null,
+        Guid? therapistId = null)
         => Customer.Create(
-            firstName ?? FirstName,
-            lastName ?? LastName,
+            firstName ?? Firstname,
+            lastName ?? Lastname,
             birthDate ?? Birthdate,
-            note ?? Note,
             address ?? Address,
             email ?? Email,
-            phoneNumber ?? PhoneNumber
-            );
+            phoneNumber ?? PhoneNumber,
+            Note,
+            therapistId);
 
 
     //Arrange
