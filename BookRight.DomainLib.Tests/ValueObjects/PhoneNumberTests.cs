aed9b4944b6a6b29b0b19fd4d87c28820e84f7e5
nico003t <nicolaihlb@gmail.com>
2026-05-04 01:46:46 +0200

    Added: Class PhoneNumberTests with testdata and Helpermethod CreatePhoneNumberWithValidData()

---
+++ BookRight.DomainLib.Tests/ValueObjects/PhoneNumberTests.cs
@@ -0,0 +1,11 @@
+﻿using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+
+namespace BookRight.DomainLib.Tests.ValueObjects;
+
+public class PhoneNumberTests
+{
+    private static string Number => "98235467";
+
+    private static PhoneNumber CreatePhoneNumberWithValidData(string? number = null) => new PhoneNumber(number ?? Number);
+}
