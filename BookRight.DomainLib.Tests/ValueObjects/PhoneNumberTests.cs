63aec9c6060c8d767764a3263551dacb5d08ec28
nico003t <nicolaihlb@gmail.com>
2026-05-04 02:00:33 +0200

    Added: Unittest Create_WithInvalidNoOfDigits_ThrowDomainException

--- BookRight.DomainLib.Tests/ValueObjects/PhoneNumberTests.cs
+++ BookRight.DomainLib.Tests/ValueObjects/PhoneNumberTests.cs
@@ -8,4 +8,18 @@ public class PhoneNumberTests
     private static string Number => "98235467";
 
     private static PhoneNumber CreatePhoneNumberWithValidData(string? number = null) => new PhoneNumber(number ?? Number);
+
+
+    // ---------------------------------------------------------
+    // 1. Create tests (Creating a PhoneNumber)
+    // ---------------------------------------------------------
+
+    [Theory]
+    [InlineData("923759023")]
+    [InlineData("9450375")]
+    public void Create_WithInvalidNoOfDigits_ThrowDomainException(string phoneNumber)
+    {
+        // Act & Assert
+        Assert.Throws<DomainException>(() => CreatePhoneNumberWithValidData(phoneNumber));
+    }
 }
