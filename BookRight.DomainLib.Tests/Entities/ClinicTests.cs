313e36d8236a80fabff8a4197308f09398c12c1e
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-04 20:15:26 +0200

    Added Address value object to create helper method and as static property

--- BookRight.DomainLib.Tests/Entities/ClinicTests.cs
+++ BookRight.DomainLib.Tests/Entities/ClinicTests.cs
@@ -9,16 +9,19 @@ public class ClinicTests
     private static string Name => "Klinik Vejle";
     private static int TreatmentRoomLimit => 5;
 
-    private static OpeningHours OpeningHours(DateTime open, DateTime close) => new OpeningHours(open, close);
+    private static OpeningHours OpeningHours(DateTime open, DateTime close) => new(open, close);
+    private static Address Address => new("Testvej 1", "1234", "FantasiBy");
 
     private static Clinic CreateWithValidData(
         string? name = null,
         int? treatmentRoomLimit = null,
-        OpeningHours? openingHours = null)
+        OpeningHours? openingHours = null,
+        Address? address = null)
         => Clinic.Create(
             name ?? Name,
             treatmentRoomLimit ?? TreatmentRoomLimit,
-            openingHours ?? OpeningHours(DateTime.Now.AddDays(1), DateTime.Now.AddDays(1))
+            openingHours ?? OpeningHours(DateTime.Now.AddDays(1), DateTime.Now.AddDays(1)),
+            address ?? Address
             );
     // ---------------------------------------------------------
     // 1. Create tests (Creating a Clinic)
