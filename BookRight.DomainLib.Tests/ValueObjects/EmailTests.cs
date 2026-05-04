d8ba2d51456298237a4f212e2f46a990ee7b2f5f
nico003t <nicolaihlb@gmail.com>
2026-05-04 09:14:14 +0200

    Added: Class EmailTests with testdata and Helpermethod CreateEmailWithValidData()

---
+++ BookRight.DomainLib.Tests/ValueObjects/EmailTests.cs
@@ -0,0 +1,17 @@
+﻿using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+
+namespace BookRight.DomainLib.Tests.ValueObjects
+{
+    public class EmailTests
+    {
+        private static string EmailAddress => "john@doe.com";
+
+        private static Email CreateEmailWithValidData(string? emailAddress = null) => new Email(emailAddress ?? EmailAddress);
+
+
+        // ---------------------------------------------------------
+        // 1. Create tests (Creating an Email)
+        // ---------------------------------------------------------
+    }
+}
