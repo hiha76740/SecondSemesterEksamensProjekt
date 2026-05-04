4c8524776095cfbc7adcf2d85bb9a304061cc50c
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-05-04 12:20:49 +0200

    Added Services folder to Domain and Application Layer

---
+++ BookRight.ApplicationLib/Services/Dummy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.ApplicationLib.Services
+{
+    internal class Dummy
+    {
+    }
+}
---
+++ BookRight.DomainLib/Services/Dummy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.DomainLib.Services
+{
+    internal class Dummy
+    {
+    }
+}
