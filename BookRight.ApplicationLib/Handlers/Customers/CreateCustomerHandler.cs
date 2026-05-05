b0bea41b594dbd80e9ba3f595afa9048cb4e9ef4
nico003t <nicolaihlb@gmail.com>
2026-05-05 11:51:55 +0200

    Added: CustomerDtos.cs and CreateCustomerCommand
    Removed: Unnecessary usings

--- BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
+++ BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+namespace BookRight.ApplicationLib.Handlers.Customers;
 
-namespace BookRight.ApplicationLib.Handlers.Customers
+public class CreateCustomerHandler
 {
-    public class CreateCustomerHandler
-    {
-    }
 }
