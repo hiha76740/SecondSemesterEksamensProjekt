2898b5d1d1d56e33cb0da1d9886116049df684b8
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-05 10:46:28 +0200

    Added Override method for OnConfiguring

---
+++ BookRight.InfrastructureLib/Persistance/BookRightDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookRight.InfrastructureLib.Persistance;
+
+public class BookRightDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured != false)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BookRightDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30"); 
+        }
+    }
+}
