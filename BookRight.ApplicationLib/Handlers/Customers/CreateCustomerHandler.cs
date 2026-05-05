4f9a25fe7db32d792890422025d766aa2b53785f
nico003t <nicolaihlb@gmail.com>
2026-05-05 13:20:59 +0200

    Added: Implemented ICreateCustomerHandler and set up repository injection in CreateCustomerHandler.

--- BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
+++ BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
@@ -1,6 +1,19 @@
 ﻿
+using BookRight.ApplicationLib.Repositories;
+using BookRight.FacadeLib.Handlers;
+
 namespace BookRight.ApplicationLib.Handlers.Customers;
 
-public class CreateCustomerHandler
+public class CreateCustomerHandler : ICreateCustomerHandler
 {
+    private readonly ICustomerRepository _customerRepository;
+    private readonly ITherapistRepository _therapistRepository;
+
+    public CreateCustomerHandler(
+        ICustomerRepository customerRepo,
+        ITherapistRepository therapistRepo)
+    {
+        _customerRepository = customerRepo;
+        _therapistRepository = therapistRepo;
+    }
 }
