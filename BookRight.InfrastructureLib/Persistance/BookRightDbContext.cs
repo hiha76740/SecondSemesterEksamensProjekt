5466a9b0960b9371d983aa744e7b45fc5585ec44
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-05 10:46:46 +0200

    Added override method for OnModelCreating

--- BookRight.InfrastructureLib/Persistance/BookRightDbContext.cs
+++ BookRight.InfrastructureLib/Persistance/BookRightDbContext.cs
@@ -11,4 +11,9 @@ public class BookRightDbContext : DbContext
             optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BookRightDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30"); 
         }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookRightDbContext).Assembly);
+    }
 }
