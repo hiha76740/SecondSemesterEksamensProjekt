2744ecefabdbe318a17bdb468f114799abd086ba
nico003t <nicolaihlb@gmail.com>
2026-05-05 12:48:53 +0200

    Added: ITherapistRepository to ApplicationLib

---
+++ BookRight.ApplicationLib/Repositories/ITherapistRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookRight.DomainLib.Entities.Therapists;
+
+namespace BookRight.ApplicationLib.Repositories
+{
+    public interface ITherapistRepository
+    {
+        Task<Therapist?> GetAsync(Guid id);
+    }
+}
