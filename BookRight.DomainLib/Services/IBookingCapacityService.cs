7107c3da6f69e59a74c90c303bc2e4996d74d3aa
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-05 08:22:56 +0200

    Added IBookingCapacityService Interface

---
+++ BookRight.DomainLib/Services/IBookingCapacityService.cs
@@ -0,0 +1,11 @@
+﻿using BookRight.DomainLib.Entities.Bookings;
+using BookRight.DomainLib.Entities.Clinics;
+using BookRight.DomainLib.ValueObjects;
+
+namespace BookRight.DomainLib.Services
+{
+    public interface IBookingCapacityService
+    {
+        bool CanCreateBooking(Clinic clinic, IEnumerable<Booking> existingBookings, TimeSlot timeSlot);
+    }
+}
