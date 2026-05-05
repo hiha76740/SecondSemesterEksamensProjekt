8fa892cc2f2bb1f7b064e6d8c508dbde00efaf98
nico003t <nicolaihlb@gmail.com>
2026-05-05 18:44:17 +0200

    Added: Implemented Handle in CreateCustomerHandler

--- BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
+++ BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
@@ -1,5 +1,8 @@
 ﻿
 using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Customers;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.DTO;
 using BookRight.FacadeLib.Handlers;
 
 namespace BookRight.ApplicationLib.Handlers.Customers;
@@ -15,5 +18,26 @@ public class CreateCustomerHandler : ICreateCustomerHandler
     {
         _customerRepository = customerRepo;
         _therapistRepository = therapistRepo;
+    }
+
+    async Task ICreateCustomerHandler.Handle(CreateCustomerCommand command)
+    {
+        
+        if (command.TherapistId.HasValue)
+        {
+            Guid therapistId = command.TherapistId.Value;
+            _ = await _therapistRepository.GetByIdAsync(therapistId)
+                ?? throw new ApplicationException("Therapist could not be found");
+        }
+
+        var address = new Address(command.Street, command.PostalCode, command.City);
+        var email = new Email(command.EmailAddress);
+        var phoneNumber = new PhoneNumber(command.PhoneNumber);
+
+        var customer = Customer.Create(command.Firstname, command.Lastname, command.Birthdate, address, email, phoneNumber, command.Note, command.TherapistId);
+
+        await _customerRepository.AddAsync(customer);
+        await _customerRepository.SaveAsync();
+        
     }
 }
