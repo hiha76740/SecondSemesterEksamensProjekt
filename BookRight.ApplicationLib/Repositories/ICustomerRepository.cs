a137c071afc49f4fa392d6484424264a0d9fa5fa
nico003t <nicolaihlb@gmail.com>
2026-05-05 12:40:01 +0200

    Added: ICustomerRepository to ApplicationLib

---
+++ BookRight.ApplicationLib/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookRight.DomainLib.Entities.Customers;
+
+namespace BookRight.ApplicationLib.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetAsync(Guid id);
+        Task AddAsync(Customer customer);
+        Task SaveAsync();
+    }
+}
