11e0d17d9f2708c2e1246949081bdd1c20ecdc6d
nico003t <nicolaihlb@gmail.com>
2026-05-05 11:43:42 +0200

    Added: Customers folder and added CreateCustomerHandler to it.

---
+++ BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.ApplicationLib.Handlers.Customers
+{
+    public class CreateCustomerHandler
+    {
+    }
+}
