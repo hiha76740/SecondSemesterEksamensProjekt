1c4cd6f8382be8aa2e32390ac65bdbe51559900e
Christian Thor Hartmeyer Fasterholt <cthf76298@edu.ucl.dk>
2026-05-05 12:24:51 +0200

    Refactor application repository interfaces

---
+++ BookRight.ApplicationLib/Repositories/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using BookRight.DomainLib.Entities.Customers;
+
+namespace BookRight.ApplicationLib.Repositories;
+
+public interface ICustomerRepository
+{
+    Task<Customer?> GetByIdAsync(Guid id);
+}
