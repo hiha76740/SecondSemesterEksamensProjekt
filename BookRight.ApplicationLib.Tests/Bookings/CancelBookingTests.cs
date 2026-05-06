1101cad88b878cb2132131639fd13d88621f91ab
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-06 16:53:51 +0200

    Added Feedback with TODO comments

--- BookRight.ApplicationLib.Tests/Bookings/CancelBookingTests.cs
+++ BookRight.ApplicationLib.Tests/Bookings/CancelBookingTests.cs
@@ -1,7 +1,6 @@
 ﻿using BookRight.ApplicationLib.Handlers.Booking;
 using BookRight.ApplicationLib.Repositories;
 using BookRight.DomainLib.Entities.Bookings;
-using BookRight.DomainLib.Enums;
 using BookRight.DomainLib.Exceptions;
 using BookRight.DomainLib.ValueObjects;
 using BookRight.FacadeLib.Commands.Booking.DTOs;
@@ -12,6 +11,12 @@ namespace BookRight.ApplicationLib.Tests.Handlers.Bookings;
 
 public class CancelBookingTests
 {
+    /* 
+     TODO:
+     Ikke nødvendigt, du skal ikke ændre i parameterne i handler test, hvorfor du blot kan hard code dem ind,
+     som jeg har gjort for dig i denne.
+     
+
     private static Guid CustomerId => Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b453b80d");
     private static Guid TreatmentId => Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b693b80d");
     private static Guid TherapistId => Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b993b80d");
@@ -23,7 +28,7 @@ public class CancelBookingTests
 
     private static IEnumerable<Booking> ExistingCustomerBookings => Array.Empty<Booking>();
     private static IEnumerable<Booking> ExistingTherapistBookings => Array.Empty<Booking>();
-
+    */
     private static TimeSlot CreateTimeSlot(int fromHour, int toHour)
     {
         return new TimeSlot(
@@ -31,47 +36,45 @@ public class CancelBookingTests
             DateTime.UtcNow.AddDays(1).Date.AddHours(toHour));
     }
 
-    private static Booking CreateWithoutOverlap(
-        TimeSlot? timeSlot = null,
-        Guid? customerId = null,
-        Guid? treatmentId = null,
-        Guid? therapistId = null,
-        Guid? clinicId = null,
-        decimal? price = null,
-        IEnumerable<Booking>? existingCustomerBookings = null,
-        IEnumerable<Booking>? existingTherapistBookings = null)
+    private static Booking CreateBooking()
     {
         return Booking.Create(
-            timeSlot ?? CreateTimeSlot(9, 10),
-            customerId ?? CustomerId,
-            treatmentId ?? TreatmentId,
-            therapistId ?? TherapistId,
-            clinicId ?? ClinicId,
-            price ?? Price,
-            existingCustomerBookings ?? ExistingCustomerBookings,
-            existingTherapistBookings ?? ExistingTherapistBookings);
+            CreateTimeSlot(9, 10),
+            Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b453b80d"),
+            Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b693b80d"),
+            Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b993b80d"),
+            Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b493c80d"),
+            550m,
+            Array.Empty<Booking>(),
+            Array.Empty<Booking>());
     }
 
     // ---------------------------------------------------------
     // 1. Handle tests (Cancelling a Booking through Application)
     // ---------------------------------------------------------
 
+    /*
+    
+    TODO: Slettes, Vi tester kun 1 ting, ellers bryder du S i SOLID
+
+
     [Fact]
     public async Task Handle_GivenValidBookingId_SetsStatusToCancelled()
     {
         // Arrange
-        Booking booking = CreateWithoutOverlap();
+        Booking booking = CreateBooking();
 
         var bookingRepositoryMock = new Mock<IBookingRepository>();
 
         bookingRepositoryMock
             .Setup(repository => repository.GetByIdAsync(booking.Id))
             .ReturnsAsync(booking);
-
-        bookingRepositoryMock
-            .Setup(repository => repository.Save())
-            .Returns(Task.CompletedTask);
-
+        
+          Ikke nødvendig
+        //bookingRepositoryMock
+        //    .Setup(repository => repository.Save())
+        //    .Returns(Task.CompletedTask);
+        
         ICancelBookingHandler handler = new CancelBookingHandler(bookingRepositoryMock.Object);
 
         var command = new CancelBookingCommand(booking.Id);
@@ -82,12 +85,13 @@ public class CancelBookingTests
         // Assert
         Assert.Equal(BookingStatus.Cancelled, booking.Status);
     }
+    */
 
     [Fact]
     public async Task Handle_GivenValidBookingId_CallsSave()
     {
         // Arrange
-        Booking booking = CreateWithoutOverlap();
+        Booking booking = CreateBooking();
 
         var bookingRepositoryMock = new Mock<IBookingRepository>();
 
@@ -95,9 +99,14 @@ public class CancelBookingTests
             .Setup(repository => repository.GetByIdAsync(booking.Id))
             .ReturnsAsync(booking);
 
-        bookingRepositoryMock
-            .Setup(repository => repository.Save())
-            .Returns(Task.CompletedTask);
+        /* 
+         
+        TODO: Dette er det du tester til slut med Verify, så det er blot fyld
+
+        //bookingRepositoryMock
+        //    .Setup(repository => repository.Save())
+        //    .Returns(Task.CompletedTask);
+        */
 
         ICancelBookingHandler handler = new CancelBookingHandler(bookingRepositoryMock.Object);
 
@@ -123,9 +132,14 @@ public class CancelBookingTests
         // Act & Assert
         await Assert.ThrowsAsync<Exceptions.ApplicationException>(() => handler.Handle(command));
 
-        bookingRepositoryMock.Verify(repository => repository.GetByIdAsync(It.IsAny<Guid>()), Times.Never);
+        /*
+        TODO:  
+        Dette behøves ikke testes, der sker ikke når programmet får en Exception (normal C# kode)
 
-        bookingRepositoryMock.Verify(repository => repository.Save(), Times.Never);
+        //bookingRepositoryMock.Verify(repository => repository.GetByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+        //bookingRepositoryMock.Verify(repository => repository.Save(), Times.Never);
+        */
     }
 
     [Fact]
@@ -135,16 +149,21 @@ public class CancelBookingTests
         var bookingRepositoryMock = new Mock<IBookingRepository>();
 
         bookingRepositoryMock
-            .Setup(repository => repository.GetByIdAsync(UnknownBookingId))
+            .Setup(r => r.GetByIdAsync(It.IsAny<Guid>()))
             .ReturnsAsync((Booking?)null);
 
         ICancelBookingHandler handler = new CancelBookingHandler(bookingRepositoryMock.Object);
 
-        var command = new CancelBookingCommand(UnknownBookingId);
+        var command = new CancelBookingCommand(Guid.NewGuid());
 
         // Act & Assert
         await Assert.ThrowsAsync<NotFoundException>(() => handler.Handle(command));
 
-        bookingRepositoryMock.Verify(repository => repository.Save(), Times.Never);
+        /*
+        TODO:  
+        Dette behøves ikke testes, der sker ikke når programmet får en Exception (normal C# kode)
+
+        //bookingRepositoryMock.Verify(repository => repository.Save(), Times.Never);
+        */
     }
 }
