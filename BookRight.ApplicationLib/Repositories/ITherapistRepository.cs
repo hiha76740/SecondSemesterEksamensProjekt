1d08e4a73d608e1d5783d270a70cf3a5c6acd4a7
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-06 16:11:32 +0200

    removed dublicate

--- BookRight.ApplicationLib/Repositories/ITherapistRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using BookRight.DomainLib.Entities.Therapists;
-
-namespace BookRight.ApplicationLib.Repositories;
-
-public interface ITherapistRepository
-{
-    Task<Therapist?> GetByIdAsync(Guid id);
-}
