c3800899adfa610731068328e40749a0d3a90625
nico003t <nicolaihlb@gmail.com>
2026-05-06 14:28:01 +0200

    Added: In ApplicationLib.Test testdata and helpermethod for instantiating a CreateCustomerCommand Dto has been added.
    ##Feature-DevOps-353-Unittest-Opret-kunde##

--- BookRight.ApplicationLib.Tests/Customers/CreateCustomerHandlerTests.cs
+++ BookRight.ApplicationLib.Tests/Customers/CreateCustomerHandlerTests.cs
@@ -1,17 +1,58 @@
 ﻿using BookRight.ApplicationLib.Handlers.Customers;
 using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.Handlers.Customers.DTOs;
 using Moq;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace BookRight.ApplicationLib.Tests.Customers;
 
 public class CreateCustomerHandlerTests
 {
+    //Testdata
+    private static string Firstname => "Anne";
+    private static string Lastname => "Andersen";
+    private static DateTime Birthdate => new DateTime(1989, 2, 12);
+    private static string Note => "Milk-allergy";
+    private static string Street => "Test Allé 28";
+    private static string PostalCode => "4321";
+    private static string City => "Testby";
+    private static string Email => "Anne.Andersen@testing.com";
+    private static string PhoneNumber => "56781234";
+
+    //Helpermethod to instatiate a CreateCustomerCommand Dto
+    private static CreateCustomerCommand CreateCustomerCommandWithValidData(
+        string? firstName = null,
+        string? lastName = null,
+        DateTime? birthDate = null,
+        string? street = null,
+        string? postalCode = null,
+        string? city = null,
+        string? email = null,
+        string? phoneNumber = null,
+        Guid? therapistId = null)
+        => new CreateCustomerCommand(
+            firstName ?? Firstname,
+            lastName ?? Lastname,
+            birthDate ?? Birthdate,
+            Note,
+            street ?? Street,
+            postalCode ?? PostalCode,
+            city ?? City,
+            email ?? Email,
+            phoneNumber ?? PhoneNumber,
+            therapistId
+            );
+
+    //Mock of relevant repositories
     private readonly Mock<ICustomerRepository> _mockCustomerRepo = new();
     private readonly Mock<ITherapistRepository> _mockTherapistRepo = new();
 
+    //SystemUnderTest
     private CreateCustomerHandler CreateSut() => new(
         _mockCustomerRepo.Object,
         _mockTherapistRepo.Object
         );
+
 }
