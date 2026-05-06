8d8e3c2fba6ebfab146a2da16d8b23fab0d23c5c
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-06 16:21:23 +0200

    Added TherapistConfiguration

---
+++ BookRight.InfrastructureLib/Persistance/EF Configurations/TherapistConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookRight.DomainLib.Entities.Therapists;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookRight.InfrastructureLib.Persistance.EF_Configurations;
+
+internal class TherapistConfiguration : IEntityTypeConfiguration<Therapist>
+{
+    public void Configure(EntityTypeBuilder<Therapist> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
