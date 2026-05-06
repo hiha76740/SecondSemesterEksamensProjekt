722e21afa74f794f03d617704087ace055e9b64b
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-06 20:04:59 +0200

    Add CreateTherapistHandler unit test class setup
    Introduced CreateTherapistHandlerTests with Moq-based repository mocking and handler initialization to support unit testing. Added necessary using directives for dependencies.

---
+++ BookRight.ApplicationLib.Tests/Therapists/CreateTherapistHandlerTest.cs
@@ -0,0 +1,23 @@
+﻿using BookRight.ApplicationLib.Handlers.Therapists;
+using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Therapists;
+using BookRight.FacadeLib.DTO;
+using Moq;
+
+namespace BookRight.ApplicationLib.Tests.Handlers.Therapists;
+
+public class CreateTherapistHandlerTests
+{
+    private readonly Mock<ITherapistRepository> _therapistRepositoryMock;
+
+    private readonly CreateTherapistHandler _handler;
+
+
+    public CreateTherapistHandlerTests()
+    {
+        _therapistRepositoryMock = new Mock<ITherapistRepository>();
+
+        _handler = new CreateTherapistHandler(
+            _therapistRepositoryMock.Object);
+    }
+}
