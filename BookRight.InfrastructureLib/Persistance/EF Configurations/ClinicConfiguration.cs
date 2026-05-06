99dcfb120a8dd27bd91095e04baf7108b2eced60
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-06 16:21:05 +0200

    Added ClinicConfiguration

---
+++ BookRight.InfrastructureLib/Persistance/EF Configurations/ClinicConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookRight.DomainLib.Entities.Clinics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookRight.InfrastructureLib.Persistance.EF_Configurations;
+
+internal class ClinicConfiguration : IEntityTypeConfiguration<Clinic>
+{
+    public void Configure(EntityTypeBuilder<Clinic> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
