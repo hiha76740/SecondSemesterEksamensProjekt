a6baa4c3d2b797b92fe0f5ffb17e0d2deed12f33
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-06 16:21:15 +0200

    Added CustomerConfiguration

---
+++ BookRight.InfrastructureLib/Persistance/EF Configurations/CustomerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookRight.DomainLib.Entities.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookRight.InfrastructureLib.Persistance.EF_Configurations;
+
+internal class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
