9ac05116106568a0f575ddc76b0c1802a3f80077
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-06 20:26:53 +0200

    Add IAddCertificationTypeHandler interface
    Introduced the IAddCertificationTypeHandler interface in the BookRight.FacadeLib.Handlers namespace with an async Handle method for AddCertificationTypeCommand. Added required using directives.

---
+++ BookRight.FacadeLib/Commands/Therapists/Interfaces/AddCetificationTypeHandler.cs
@@ -0,0 +1,8 @@
+﻿using BookRight.FacadeLib.DTO;
+
+namespace BookRight.FacadeLib.Handlers;
+
+public interface IAddCertificationTypeHandler
+{
+    Task Handle(AddCertificationTypeCommand command);
+}
