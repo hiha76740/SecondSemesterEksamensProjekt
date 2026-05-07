dce83f81642069630d70dc238e8b3c4d697b7fee
Nicolai Helby <nicolaihlb@gmail.com>
2026-05-07 18:01:15 +0200

    Feature-ChangeCustomerInfoHandler-Add
    Added ChangeCustomerInfoHandler.cs with a skeleton of Handle

---
+++ BookRight.ApplicationLib/Handlers/Customers/ChangeCustomerInfoHandler.cs
@@ -0,0 +1,14 @@
+﻿
+using BookRight.ApplicationLib.Repositories;
+using BookRight.FacadeLib.Commands.Customers.DTOs;
+using BookRight.FacadeLib.Commands.Customers.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Customers;
+
+public class ChangeCustomerInfoHandler(ICustomerRepository customerRepository, ITherapistRepository therapistRepository) : IChangeCustomerInfoHandler
+{
+    async Task IChangeCustomerInfoHandler.Handle(ChangeCustomerInfoCommand command)
+    {
+
+    }
+}
