b1cd79e9bbb8975dd6c26c405a6ee0580011378f
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-07 07:14:38 +0200

    feature-Change-CreateTherapistHandlerTests
    Renamed file to match class name, and rewrote whole file

---
+++ BookRight.ApplicationLib.Tests/Therapists/CreateTherapistHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using BookRight.ApplicationLib.Handlers.Therapists;
+using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Therapists;
+using BookRight.FacadeLib.Commands.Therapists.DTOs;
+using BookRight.FacadeLib.Commands.Therapists.Interfaces;
+using Moq;
+
+namespace BookRight.ApplicationLib.Tests.Handlers.Therapists;
+
+public class CreateTherapistHandlerTests
+{
+    // ---------------------------------------------------------
+    // 1. Handle tests (Creates a therapist)
+    // ---------------------------------------------------------
+
+    [Fact]
+    public async Task Handle_GivenValidCommand_CallsAddAndSave()
+    {
+        var mockTherapistRepo = new Mock<ITherapistRepository>();
+
+        var handler = new CreateTherapistHandler(mockTherapistRepo.Object) as ICreateTherapistHandler;
+
+        var command = new CreateTherapistCommand(
+            "AUTH123",
+            "John Doe",
+            550,
+            "Testvej 1",
+            "6700",
+            "Esbjerg",
+            "test@test.dk",
+            "12345678",
+            new List<Guid> { Guid.Parse("d62f5c2d-a5e6-4523-902d-108acac956c8") });
+        // Act
+        await handler.Handle(command);
+
+
+        // Assert
+        mockTherapistRepo.Verify(r => r.AddAsync(It.IsAny<Therapist>()),Times.Once);
+        mockTherapistRepo.Verify(r => r.SaveAsync(),Times.Once);
+    }
+}
