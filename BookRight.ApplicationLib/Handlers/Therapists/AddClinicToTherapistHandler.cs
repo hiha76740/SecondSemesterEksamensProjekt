7bb78487ccf03958be961ea9c5f7e62f45d084ac
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-07 12:38:25 +0200

    Add AddClinicToTherapistHandler class skeleton
    Introduced AddClinicToTherapistHandler in the Handlers.Therapists namespace, implementing IAddClinicToTherapistHandler. The class is constructed with ITherapistRepository and IClinicRepository dependencies. No method implementations are included yet.

---
+++ BookRight.ApplicationLib/Handlers/Therapists/AddClinicToTherapistHandler.cs
@@ -0,0 +1,14 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Exceptions;
+using BookRight.FacadeLib.Commands.Therapists.DTOs;
+using BookRight.FacadeLib.Commands.Therapists.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Therapists;
+
+public class AddClinicToTherapistHandler(
+    ITherapistRepository therapistRepository,
+    IClinicRepository clinicRepository)
+    : IAddClinicToTherapistHandler
+{
+    
+}
