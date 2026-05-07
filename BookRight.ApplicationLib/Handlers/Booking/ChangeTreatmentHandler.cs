722b9381b5ffdbf6653d2884115b65da8378ea48
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-07 16:59:46 +0200

    removed unused using

--- BookRight.ApplicationLib/Handlers/Booking/ChangeTreatmentHandler.cs
+++ BookRight.ApplicationLib/Handlers/Booking/ChangeTreatmentHandler.cs
@@ -1,5 +1,4 @@
 ﻿using BookRight.ApplicationLib.Repositories;
-using BookRight.DomainLib.Enums;
 using BookRight.DomainLib.Exceptions;
 using BookRight.FacadeLib.Commands.Booking.DTOs;
 using BookRight.FacadeLib.Commands.Booking.Interfaces;
