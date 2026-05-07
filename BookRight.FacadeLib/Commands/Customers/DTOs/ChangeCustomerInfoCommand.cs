52ddfdc687c330bba2c166b90caa2c0d216ed064
Nicolai Helby <nicolaihlb@gmail.com>
2026-05-07 17:11:16 +0200

    Feature-ChangeCustomerInfoCommand-Add
    Added ChangeCustomerInfoCommand.cs

---
+++ BookRight.FacadeLib/Commands/Customers/DTOs/ChangeCustomerInfoCommand.cs
@@ -0,0 +1,17 @@
+﻿
+namespace BookRight.FacadeLib.Commands.Customers.DTOs;
+
+// === Request DTO for ChangeCustomerInfoHandler ===
+public record ChangeCustomerInfoCommand(
+    Guid CustomerId,
+    string Firstname,
+    string Lastname,
+    DateTime Birthdate,
+    string Note,
+    string Street,
+    string PostalCode,
+    string City,
+    string EmailAddress,
+    string PhoneNumber,
+    Guid? PreferredTherapist
+    );
