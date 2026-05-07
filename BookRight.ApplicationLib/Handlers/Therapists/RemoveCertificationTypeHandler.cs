89ba526082ff030b4c282b78cffef430f280de3e
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-07 09:48:52 +0200

    Feature-DevOps-360-Remove-Certification-Type-Handler
    Add RemoveCertificationTypeHandler class skeleton
    
    Introduce RemoveCertificationTypeHandler with dependencies and constructor. No business logic implemented yet; sets up structure for future handler functionality.

---
+++ BookRight.ApplicationLib/Handlers/Therapists/RemoveCertificationTypeHandler.cs
@@ -0,0 +1,21 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Enums;
+using BookRight.DomainLib.Exceptions;
+using BookRight.FacadeLib.Commands.Therapists.DTOs;
+using BookRight.FacadeLib.Commands.Therapists.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Therapists;
+
+public class RemoveCertificationTypeHandler : IRemoveCertificationTypeHandler
+{
+    private readonly ITherapistRepository _therapistRepository;
+
+    public RemoveCertificationTypeHandler(
+        ITherapistRepository therapistRepository)
+    {
+        _therapistRepository = therapistRepository;
+    }
+
+
+   
+}
