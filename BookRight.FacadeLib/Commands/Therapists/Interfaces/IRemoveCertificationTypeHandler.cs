3f52345a916d19a300861122da8cce5f958fb398
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-07 09:37:27 +0200

    Feature-DevOps-363-Remove-Certification-Type-Interface
    Add IRemoveCertificationTypeHandler interface
    
    Introduced IRemoveCertificationTypeHandler with a Handle method for processing RemoveCertificationTypeCommand. Added required using directive for DTOs.

---
+++ BookRight.FacadeLib/Commands/Therapists/Interfaces/IRemoveCertificationTypeHandler.cs
@@ -0,0 +1,8 @@
+﻿using Susing BookRight.FacadeLib.Commands.Therapists.DTOs;
+
+namespace BookRight.FacadeLib.Commands.Therapists.Interfaces;
+
+public interface IRemoveCertificationTypeHandler
+{
+    Task Handle(RemoveCertificationTypeCommand command);
+}
