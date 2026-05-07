6d98f7ae685723706f9fee1b8495d4eac7fc359a
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-07 13:03:46 +0200

    Add handler to remove clinic from therapist
    Implemented RemoveClinicFromTherapistHandler to handle dissociation of a clinic from a therapist. Includes validation for therapist and clinic existence, checks association, and throws NotFoundException as needed.

--- BookRight.ApplicationLib/Handlers/Therapists/RemoveClinicFromTherapistHandler.cs
+++ BookRight.ApplicationLib/Handlers/Therapists/RemoveClinicFromTherapistHandler.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Exceptions;
+using BookRight.FacadeLib.Commands.Therapists.DTOs;
+using BookRight.FacadeLib.Commands.Therapists.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Therapists;
+
+public class RemoveClinicFromTherapistHandler(
+    ITherapistRepository therapistRepository,
+    IClinicRepository clinicRepository)
+    : IRemoveClinicFromTherapistHandler
+{
+    async Task IRemoveClinicFromTherapistHandler.Handle(
+        RemoveClinicFromTherapistCommand command)
+    {
+        var therapist = await therapistRepository
+            .GetByIdAsync(command.TherapistId)
+            ?? throw new NotFoundException(
+                "Therapist could not be found");
+
+
+        _ = await clinicRepository
+            .GetByIdAsync(command.ClinicId)
+            ?? throw new NotFoundException(
+                "Clinic could not be found");
+
+
+        if (therapist.AssociatedClinicIds.Contains(command.ClinicId) == false)
+            throw new NotFoundException(
+                "Therapist is not associated with this clinic");
+
+
+        therapist.AssociatedClinicIds.Remove(command.ClinicId);
+
+
+        await therapistRepository.SaveAsync();
+    }
+}
