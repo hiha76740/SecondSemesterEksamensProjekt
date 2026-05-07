be1f73b4ac4e28d96924808c4ea7eb76310e61b5
nico003t <nicolaihlb@gmail.com>
2026-05-07 14:27:40 +0200

    Refactor-CreateCustomerHandler-Changed-To-Use-Primary-Constructor

--- BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
+++ BookRight.ApplicationLib/Handlers/Customers/CreateCustomerHandler.cs
@@ -8,26 +8,15 @@ using BookRight.FacadeLib.Commands.Customers.Interfaces;
 
 namespace BookRight.ApplicationLib.Handlers.Customers;
 
-public class CreateCustomerHandler : ICreateCustomerHandler
+public class CreateCustomerHandler(ICustomerRepository customerRepository, ITherapistRepository therapistRepository) : ICreateCustomerHandler
 {
-    private readonly ICustomerRepository _customerRepository;
-    private readonly ITherapistRepository _therapistRepository;
-
-    public CreateCustomerHandler(
-        ICustomerRepository customerRepo,
-        ITherapistRepository therapistRepo)
-    {
-        _customerRepository = customerRepo;
-        _therapistRepository = therapistRepo;
-    }
-
     async Task ICreateCustomerHandler.Handle(CreateCustomerCommand command)
     {
         
         if (command.TherapistId.HasValue)
         {
             Guid therapistId = command.TherapistId.Value;
-            _ = await _therapistRepository.GetByIdAsync(therapistId)
+            _ = await therapistRepository.GetByIdAsync(therapistId)
                 ?? throw new NotFoundException("Therapist could not be found");
         }
 
@@ -37,8 +26,7 @@ public class CreateCustomerHandler : ICreateCustomerHandler
 
         var customer = Customer.Create(command.Firstname, command.Lastname, command.Birthdate, address, email, phoneNumber, command.Note, command.TherapistId);
 
-        await _customerRepository.AddAsync(customer);
-        await _customerRepository.SaveAsync();
-        
+        await customerRepository.AddAsync(customer);
+        await customerRepository.SaveAsync();
     }
 }
