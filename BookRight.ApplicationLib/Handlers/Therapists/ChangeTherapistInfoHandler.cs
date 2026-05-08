cc2761f4c8f3738d22bdcbfd8269ca62425e6c80
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-08 14:56:52 +0200

    add ChangeTherapistInfoHandler skeleton
    add handler implementation for therapist info updates
    inject therapist repository dependency
    implement IChangeTherapistInfoHandler interface
    prepare foundation for therapist update logic

---
+++ BookRight.ApplicationLib/Handlers/Therapists/ChangeTherapistInfoHandler.cs
@@ -0,0 +1,13 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Enums;
+using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.Commands.Therapists.DTOs;
+using BookRight.FacadeLib.Commands.Therapists.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Therapists;
+
+public class ChangeTherapistInfoHandler(ITherapistRepository therapistRepository) : IChangeTherapistInfoHandler
+{
+   
+}
