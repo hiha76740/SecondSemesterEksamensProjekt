9c76eda8b28f4bf5f636029e5b08380b4187d8f7
Christian Thor Hartmeyer Fasterholt <cthf76298@edu.ucl.dk>
2026-05-08 10:55:58 +0200

    Tasks 131-177-263-264 - Add create booking handler

---
+++ BookRight.ApplicationLib/Handlers/Booking/CreateBookingHandler.cs
@@ -0,0 +1,72 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.Services;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.Commands.Booking.DTOs;
+using BookRight.FacadeLib.Commands.Booking.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Booking;
+
+public class CreateBookingHandler(
+    IBookingRepository bookingRepository,
+    ICustomerRepository customerRepository,
+    ITherapistRepository therapistRepository,
+    IClinicRepository clinicRepository,
+    ITreatmentRepository treatmentRepository,
+    IBookingCapacityService bookingCapacityService) : ICreateBookingHandler
+{
+    async Task ICreateBookingHandler.Handle(CreateBookingCommand command)
+    {
+        if (command.CustomerId == Guid.Empty)
+            throw new Exceptions.ApplicationException("CustomerId cannot be empty.");
+
+        if (command.TreatmentId == Guid.Empty)
+            throw new Exceptions.ApplicationException("TreatmentId cannot be empty.");
+
+        if (command.TherapistId == Guid.Empty)
+            throw new Exceptions.ApplicationException("TherapistId cannot be empty.");
+
+        if (command.ClinicId == Guid.Empty)
+            throw new Exceptions.ApplicationException("ClinicId cannot be empty.");
+
+        var customer = await customerRepository.GetByIdAsync(command.CustomerId)
+            ?? throw new NotFoundException("Customer could not be found.");
+
+        var therapist = await therapistRepository.GetByIdAsync(command.TherapistId)
+            ?? throw new NotFoundException("Therapist could not be found.");
+
+        var clinic = await clinicRepository.GetByIdAsync(command.ClinicId)
+            ?? throw new NotFoundException("Clinic could not be found.");
+
+        var treatment = await treatmentRepository.GetByIdAsync(command.TreatmentId)
+            ?? throw new NotFoundException("Treatment could not be found.");
+
+        if (therapist.CertificationTypes.Contains(treatment.CertificationRequired) == false)
+            throw new Exceptions.ApplicationException("Therapist is not qualified for this treatment.");
+
+        var timeSlot = new TimeSlot(command.From, command.To);
+
+        var customerBookings = await bookingRepository.GetAllBookingsByIdAsync(customer.Id);
+
+        var therapistBookings = await bookingRepository.GetAllBookingsByIdAsync(therapist.Id);
+
+        var clinicBookings = await bookingRepository.GetAllBookingsByIdAsync(clinic.Id);
+
+        if (bookingCapacityService.CanCreateBooking(clinic, clinicBookings, timeSlot) == false)
+            throw new Exceptions.ApplicationException("Clinic capacity was exceeded.");
+
+        var booking = BookRight.DomainLib.Entities.Bookings.Booking.Create(
+            timeSlot,
+            customer.Id,
+            treatment.Id,
+            therapist.Id,
+            clinic.Id,
+            treatment.Price,
+            customerBookings,
+            therapistBookings);
+
+        await bookingRepository.AddAsync(booking);
+
+        await bookingRepository.Save();
+    }
+}
---
+++ BookRight.FacadeLib/Commands/Booking/DTOs/CreateBookingCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace BookRight.FacadeLib.Commands.Booking.DTOs;
+
+// === Request DTO'er til Use Cases (commands) ===
+
+public record CreateBookingCommand(
+    Guid CustomerId,
+    Guid TreatmentId,
+    Guid TherapistId,
+    Guid ClinicId,
+    DateTime From,
+    DateTime To);
