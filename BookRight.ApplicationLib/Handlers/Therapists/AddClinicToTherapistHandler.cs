29a2739266d6aa46790351c8b8c852d19c94b552
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-08 06:05:34 +0200

    Bug-AddClinicToTherapistHandler-AddAssociatedClinic
    Corrected so adding a clinic to therapist goes through correct method in aggregate root.

--- BookRight.ApplicationLib/Handlers/Therapists/AddClinicToTherapistHandler.cs
+++ BookRight.ApplicationLib/Handlers/Therapists/AddClinicToTherapistHandler.cs
@@ -5,29 +5,17 @@ using BookRight.FacadeLib.Commands.Therapists.Interfaces;
 
 namespace BookRight.ApplicationLib.Handlers.Therapists;
 
-public class AddClinicToTherapistHandler(
-    ITherapistRepository therapistRepository,
-    IClinicRepository clinicRepository)
-    : IAddClinicToTherapistHandler
+public class AddClinicToTherapistHandler(ITherapistRepository therapistRepository, IClinicRepository clinicRepository) : IAddClinicToTherapistHandler
 {
-    async Task IAddClinicToTherapistHandler.Handle(
-        AddClinicToTherapistCommand command)
+    async Task IAddClinicToTherapistHandler.Handle(AddClinicToTherapistCommand command)
     {
-        var therapist = await therapistRepository
-            .GetByIdAsync(command.TherapistId)
-            ?? throw new NotFoundException(
-                "Therapist could not be found");
+        var therapist = await therapistRepository.GetByIdAsync(command.TherapistId)
+            ?? throw new NotFoundException("Therapist could not be found");
 
+        _ = await clinicRepository.GetByIdAsync(command.ClinicId)
+            ?? throw new NotFoundException("Clinic could not be found");
 
-        _ = await clinicRepository
-            .GetByIdAsync(command.ClinicId)
-            ?? throw new NotFoundException(
-                "Clinic could not be found");
-
-
-
-        therapist.AssociatedClinicIds.Add(command.ClinicId);
-
+        therapist.AddAssociatedClinic(command.ClinicId);
 
         await therapistRepository.SaveAsync();
     }
