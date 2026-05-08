86da96f07883643a2a6f68522fea97cef0cdf3ad
Nicolai Helby <nicolaihlb@gmail.com>
2026-05-08 12:44:14 +0200

    Feature-ChangeCustomerInfoHandlerTests-Add-Mock-and-CreateSut
    Added mock of relevant repositories and CreateSut() to create SystemUnderTest

--- BookRight.ApplicationLib.Tests/Customers/ChangeCustomerInfoHandlerTests.cs
+++ BookRight.ApplicationLib.Tests/Customers/ChangeCustomerInfoHandlerTests.cs
@@ -1,5 +1,9 @@
 ﻿
+using BookRight.ApplicationLib.Handlers.Customers;
+using BookRight.ApplicationLib.Repositories;
 using BookRight.FacadeLib.Commands.Customers.DTOs;
+using BookRight.FacadeLib.Commands.Customers.Interfaces;
+using Moq;
 
 namespace BookRight.ApplicationLib.Tests.Customers;
 
@@ -20,5 +24,13 @@ public class ChangeCustomerInfoHandlerTests
             "96538562",
             preferredTherapist
             );
+
+    // Mock of relevant repositories
+    private readonly Mock<ICustomerRepository> _mockCustomerRepo = new();
+    private readonly Mock<ITherapistRepository> _mockTherapistRepo = new();
+
+    // SystemUnderTest
+    private IChangeCustomerInfoHandler CreateSut() => new ChangeCustomerInfoHandler(_mockCustomerRepo.Object, _mockTherapistRepo.Object);
+
 
 }
