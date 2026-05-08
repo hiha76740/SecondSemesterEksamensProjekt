a0c5f975801708a6da671f81d22d39dc2ff2ba7f
kirstine Amalie <kirstinekorsgaard2006@gmail.com>
2026-05-08 15:22:01 +0200

    add test class for ChangeTherapistInfoHandler
    create unit test suite for therapist info update handler
    prepare test structure for therapist profile update scenarios
    add foundation for validation and persistence tests
    organize handler tests under therapists application tests

---
+++ BookRight.ApplicationLib.Tests/Therapists/ChangeTherapistInfoTest.cs
@@ -0,0 +1,16 @@
+﻿using BookRight.ApplicationLib.Handlers.Therapists;
+using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Therapists;
+using BookRight.DomainLib.Enums;
+using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.Commands.Therapists.DTOs;
+using BookRight.FacadeLib.Commands.Therapists.Interfaces;
+using Moq;
+
+namespace BookRight.ApplicationLib.Tests.Handlers.Therapists;
+
+public class ChangeTherapistInfoHandlerTests
+{
+
+}
