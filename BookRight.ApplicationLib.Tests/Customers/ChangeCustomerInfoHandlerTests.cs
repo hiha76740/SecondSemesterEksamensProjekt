4b552eeb3c34e4f668f4245adc3f3b2d28cc41d5
Nicolai Helby <nicolaihlb@gmail.com>
2026-05-08 12:20:34 +0200

    Feature-ChangeCustomerInfoHandlerTests-Add
    Added ChangeCustomerInfoHandlerTests.cs and a helpermethod for instatiating a ChangeCustomerInfoCommand Dto

---
+++ BookRight.ApplicationLib.Tests/Customers/ChangeCustomerInfoHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿
+using BookRight.FacadeLib.Commands.Customers.DTOs;
+
+namespace BookRight.ApplicationLib.Tests.Customers;
+
+public class ChangeCustomerInfoHandlerTests
+{
+    //Helpermethod to instatiate a CreateCustomerCommand Dto
+    private static ChangeCustomerInfoCommand ChangeCustomerInfoCommandWithValidData(Guid? preferredTherapist = null)
+        => new ChangeCustomerInfoCommand(
+            CustomerId: Guid.NewGuid(),
+            "Torben",
+            "Svendsen",
+            new DateTime(1956, 7, 16),
+            "Gets easily confused",
+            "Niels Bohrs Gade 43",
+            "6534",
+            "Testlev",
+            "TorbenS@testing.com",
+            "96538562",
+            preferredTherapist
+            );
+
+}
