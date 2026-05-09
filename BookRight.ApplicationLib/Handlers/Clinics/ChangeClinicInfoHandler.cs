068cb9da9a701dcfa07161308a0cac7556d6217d
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-09 07:09:40 +0200

    feature-DevOps-321-ChangeClinicInfoHandler-Added-impl-of-Interface-IChangeClinicInfoHandler

---
+++ BookRight.ApplicationLib/Handlers/Clinics/ChangeClinicInfoHandler.cs
@@ -0,0 +1,44 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.Commands.Clinics.DTOs;
+using BookRight.FacadeLib.Commands.Clinics.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Clinics;
+
+public class ChangeClinicInfo(IClinicRepository clinicRepository) : IChangeClinicInfoHandler
+{
+    async Task IChangeClinicInfoHandler.Handle(ChangeClinicInfoCommand command)
+    {
+        bool changesMade = false;
+
+        var clinic = await clinicRepository.GetByIdAsync(command.ClinicId)
+            ?? throw new NotFoundException("Clinic not found");
+
+        var address = new Address(command.Street, command.PostalCode, command.City);
+        var openingHours = new OpeningHours(command.OpenHour, command.CloseHour);
+
+        if (clinic.Address != address)
+        {
+            clinic.ChangeAddress(address);
+            changesMade = true;
+        }
+
+        if (clinic.TreatmentRoomLimit != command.TreatmentRoomLimit)
+        {
+            clinic.ChangeTreatmentRoomLimit(command.TreatmentRoomLimit);
+            changesMade = true;
+        }
+
+        if (clinic.OpeningHours != openingHours)
+        {
+            clinic.ChangeOpeningHours(openingHours);
+            changesMade = true;
+        }
+
+        if (changesMade == true)
+        {
+            await clinicRepository.SaveAsync();
+        }
+    }
+}
