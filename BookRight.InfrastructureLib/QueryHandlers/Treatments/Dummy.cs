23326c50edc1693f240361ba65a04900f132d942
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-09 09:40:21 +0200

    Refactor-InfrastructureLib-Added-New-Folder-Structure

---
+++ BookRight.InfrastructureLib/QueryHandlers/Treatments/Dummy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.InfrastructureLib.QueryHandlers.Treatments
+{
+    internal class Dummy
+    {
+    }
+}
