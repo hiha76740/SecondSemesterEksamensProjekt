65f576c7b42f6cceef81c79e640fd54196a9060b
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-09 07:06:12 +0200

    feature-DevOps-305-CreateClinicCommand-Added-Command-DTO

---
+++ BookRight.FacadeLib/Commands/Clinics/DTOs/CreateClinicCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace BookRight.FacadeLib.Commands.Clinics.DTOs;
+
+public record CreateClinicCommand(string Name, string Street, string PostalCode, string City, int TreatmentRoomLimit, DateTime OpeningHour, DateTime CloseingHour);
