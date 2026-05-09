51d85259b4119ecdf40c9fdaaa742b262b450b7e
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-09 07:06:51 +0200

    feature-DevOps-305-CreateClinicHandler-Added-implementation-Of-Interface-ICreateClinicHandler

---
+++ BookRight.ApplicationLib/Handlers/Clinics/CreateClinicHandler.cs
@@ -0,0 +1,21 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Clinics;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.Commands.Clinics.DTOs;
+using BookRight.FacadeLib.Commands.Clinics.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Clinics;
+
+public class CreateClinicHandler(IClinicRepository clinicRepository) : ICreateClinicHandler
+{
+    async Task ICreateClinicHandler.Handle(CreateClinicCommand command)
+    {
+        var address = new Address(command.Street, command.PostalCode, command.City);
+        var openingHours = new OpeningHours(command.OpeningHour, command.CloseingHour);
+
+        var clinic = Clinic.Create(command.Name, command.TreatmentRoomLimit, openingHours, address);
+
+        await clinicRepository.AddAsync(clinic);
+        await clinicRepository.SaveAsync();
+    }
+}
