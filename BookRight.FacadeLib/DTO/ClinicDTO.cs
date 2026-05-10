f00a465afe7a13f09d729e1f4cca21a44840b7e1
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-10 17:27:07 +0200

    feature-ClinicDTO-Added-DTO

---
+++ BookRight.FacadeLib/DTO/ClinicDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BookRight.FacadeLib.DTO;
+
+public record ClinicDTO(
+    string Name,
+    int TreatmentRoomLimit,
+    DateTime OpeningTime,
+    DateTime CloseingTime,
+    string Street,
+    string PostalCode,
+    string City
+    );
