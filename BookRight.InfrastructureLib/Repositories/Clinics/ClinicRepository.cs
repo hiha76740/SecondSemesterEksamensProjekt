58b45ae98031c5cccabb089e2f56f1c676abbcdf
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-10 18:21:03 +0200

    feature-ClinicRepository-implemented-IClinicRepository-Interface

---
+++ BookRight.InfrastructureLib/Repositories/Clinics/ClinicRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Clinics;
+using BookRight.InfrastructureLib.Persistance.DbContextFiles;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRight.InfrastructureLib.Repositories.Clinics;
+
+public class ClinicRepository(BookRightDbContext db) : IClinicRepository
+{
+    async Task IClinicRepository.AddAsync(Clinic clinic)
+    {
+        await db.AddAsync(clinic);
+    }
+
+    async Task<Clinic?> IClinicRepository.GetByIdAsync(Guid id)
+    {
+        return await db.Clinics
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    async Task IClinicRepository.SaveAsync()
+    {
+        await db.SaveChangesAsync();
+    }
+}
