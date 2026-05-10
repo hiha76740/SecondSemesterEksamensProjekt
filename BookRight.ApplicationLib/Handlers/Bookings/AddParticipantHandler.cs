f9c1ac9f52b8d27a72acb11b9594cfb492ada5d3
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-10 18:36:24 +0200

    feature-AddParticipantHandler-Added
    Added AddParticipantHandler with interface and command

---
+++ BookRight.ApplicationLib/Handlers/Bookings/AddParticipantHandler.cs
@@ -0,0 +1,22 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Exceptions;
+using BookRight.FacadeLib.Commands.Booking.DTOs;
+using BookRight.FacadeLib.Commands.Booking.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Bookings;
+
+public class AddParticipantHandler(IBookingRepository bookingRepository, ICustomerRepository customerRepository) : IAddParticipantHandler
+{
+    async Task IAddParticipantHandler.Handle(AddParticipantCommand command)
+    {
+        var booking = await bookingRepository.GetByIdAsync(command.BookingId)
+            ?? throw new NotFoundException("Booking not found");
+
+        _ = await customerRepository.GetByIdAsync(command.CustomerId)
+            ?? throw new NotFoundException("Customer not found");
+
+        booking.AddParticipant(command.CustomerId);
+
+        await bookingRepository.SaveAsync();
+    }
+}
---
+++ BookRight.FacadeLib/Commands/Booking/Interfaces/IAddParticipantHandler.cs
@@ -0,0 +1,8 @@
+﻿using BookRight.FacadeLib.Commands.Booking.DTOs;
+
+namespace BookRight.FacadeLib.Commands.Booking.Interfaces;
+
+public interface IAddParticipantHandler
+{
+    Task Handle(AddParticipantCommand command);
+}
