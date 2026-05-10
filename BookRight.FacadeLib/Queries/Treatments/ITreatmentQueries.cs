10c986a21593c962d0c9bbae60e24e497eaa9d12
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-10 17:26:27 +0200

    feature-ITreatmentQueries-Added-Interface

---
+++ BookRight.FacadeLib/Queries/Treatments/ITreatmentQueries.cs
@@ -0,0 +1,9 @@
+﻿using BookRight.FacadeLib.DTO;
+
+namespace BookRight.FacadeLib.Queries.Treatments;
+
+public interface ITreatmentQueries
+{
+    Task<TreatmentDTO?> GetByIdAsync(Guid Id);
+    Task<IReadOnlyList<TreatmentDTO>> GetAllAsync();
+}
