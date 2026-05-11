93738cf3e0e3640b76304d8c84f470fbf8b4478e
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-11 06:40:29 +0200

    feature-OpeningHour-Added-Entity
    Changed OpeningHours from Value object to a Entity

---
+++ BookRight.DomainLib/Entities/Clinics/OpeningHour.cs
@@ -0,0 +1,40 @@
+﻿using BookRight.DomainLib.Enums;
+
+namespace BookRight.DomainLib.Entities.Clinics;
+
+public class OpeningHour: Entity
+{
+    public Weekdays Weekday { get; init; }
+    public TimeOnly? OpeningTime { get; private set; }
+    public TimeOnly? CloseingTime { get; private set; }
+    public bool IsClosed { get; private set; }
+
+
+    internal void ChangeOpeningHourTime(TimeOnly? openingTime, TimeOnly? closingTime, bool isClosed)
+    {
+        ApplyClosedState(isClosed, openingTime, closingTime);
+    }
+
+    internal OpeningHour(Weekdays weekday, TimeOnly? openingTime, TimeOnly? closingTime, bool isClosed)
+    {
+        Weekday = weekday;
+
+        ApplyClosedState(isClosed,openingTime, closingTime);        
+    }
+
+    private void ApplyClosedState(bool isClosed, TimeOnly? openingTime, TimeOnly? closingTime)
+    {
+        if (isClosed == true)
+        {
+            OpeningTime = null;
+            CloseingTime = null;
+            IsClosed = isClosed;
+        }
+        else
+        {
+            OpeningTime = openingTime;
+            CloseingTime = closingTime;
+            IsClosed = isClosed;
+        }
+    }
+}
