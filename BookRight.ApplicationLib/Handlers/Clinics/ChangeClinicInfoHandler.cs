4e99114f82710c53e4ac93fc862339d77fb72524
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-11 07:19:23 +0200

    feature-ChangeClinicInfoHandler-Changed-To-Add-Support-For-New-OpeningHour-Entity

--- BookRight.ApplicationLib/Handlers/Clinics/ChangeClinicInfoHandler.cs
+++ BookRight.ApplicationLib/Handlers/Clinics/ChangeClinicInfoHandler.cs
@@ -1,4 +1,6 @@
 ﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Clinics;
+using BookRight.DomainLib.Enums;
 using BookRight.DomainLib.Exceptions;
 using BookRight.DomainLib.ValueObjects;
 using BookRight.FacadeLib.Commands.Clinics.DTOs;
@@ -16,7 +18,32 @@ public class ChangeClinicInfoHandler(IClinicRepository clinicRepository) : IChan
             ?? throw new NotFoundException("Clinic not found");
 
         var address = new Address(command.Street, command.PostalCode, command.City);
-        var openingHours = new OpeningHours(command.OpenHour, command.CloseHour);
+
+
+        foreach (var oh in command.OpeningHours)
+        {
+            bool exsists = Enum.TryParse<Weekdays>(oh.Weekday, out var weekday);
+
+            if (exsists == false)
+                throw new NotFoundException($"Weekday {oh.Weekday} was not found");
+
+
+            var changed = clinic.OpeningHours
+                .Any(x => x.Weekday == weekday &&
+                x.OpeningTime != oh.OpeningTime ||
+                x.CloseingTime != oh.ClosingTime);
+
+
+            if (changed == true)
+            {
+                var id = clinic.OpeningHours.Where(x => x.Weekday == weekday).Select(x => x.Id).FirstOrDefault();
+
+                var opningHourInput = new OpeningHourInput(weekday, oh.OpeningTime, oh.ClosingTime, oh.IsClosed);
+
+                clinic.ChangeOpeningHour(id, opningHourInput);
+                changesMade = true;
+            }
+        }
 
         if (clinic.Address != address)
         {
@@ -27,12 +54,6 @@ public class ChangeClinicInfoHandler(IClinicRepository clinicRepository) : IChan
         if (clinic.TreatmentRoomLimit != command.TreatmentRoomLimit)
         {
             clinic.ChangeTreatmentRoomLimit(command.TreatmentRoomLimit);
-            changesMade = true;
-        }
-
-        if (clinic.OpeningHours != openingHours)
-        {
-            clinic.ChangeOpeningHours(openingHours);
             changesMade = true;
         }
 
