eb969e9b27190e4c748581f35121b85b15ecde58
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-05-11 12:13:23 +0200

    feature-PriceCalculator-Added-Strategy-Interface-And-DTOs

---
+++ BookRight.DomainLib/Discounts/IDiscountStrategy.cs
@@ -0,0 +1,9 @@
+﻿using BookRight.DomainLib.Enums;
+
+namespace BookRight.DomainLib.Discounts;
+
+public interface IDiscountStrategy
+{
+    DiscountTypes discountTypes { get; }
+    PriceCalculatorResult CalculatePrice(PriceCalculatorInput input);
+}
---
+++ BookRight.DomainLib/Discounts/PriceCalculatorInput.cs
@@ -0,0 +1,9 @@
+﻿namespace BookRight.DomainLib.Discounts;
+
+public record PriceCalculatorInput(
+    decimal NormalPrice,
+    DateTime BookingDate,
+    DateTime CustomerBirthdate,
+    decimal CustomerBookingsLast12Months,
+    int NumberOfBirthdayDiscountUsed
+    );
