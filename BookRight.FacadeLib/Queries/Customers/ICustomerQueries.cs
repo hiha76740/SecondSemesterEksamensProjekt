c89555e4784e7319bf005ec6a86a10bd0342dd96
Nicolai Helby (nico003t) <nicolaihlb@gmail.com>
2026-05-11 18:38:12 +0200

    Feature-CustomerQuery-Add-ICustomerQueries
    Added ICustomerQueries.cs to BookRight.FacadeLib and removed Dummy.cs in same directory

---
+++ BookRight.FacadeLib/Queries/Customers/ICustomerQueries.cs
@@ -0,0 +1,9 @@
+﻿using BookRight.FacadeLib.DTO;
+
+namespace BookRight.FacadeLib.Queries.Customers;
+
+public interface ICustomerQueries
+{
+    Task<CustomerDTO?> GetByIdAsync(Guid Id);
+    Task<IReadOnlyList<CustomerDTO>> GetAllAsync();
+}
