b634201ab87981aff4dc0e98f63c8766537b5c15
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-05-11 12:28:54 +0200

    feature-DatabaseDI-Added-DI-Method

--- BookRight.Web/DependencyInjections/DatabaseDI.cs
+++ BookRight.Web/DependencyInjections/DatabaseDI.cs
@@ -1,6 +1,17 @@
-﻿namespace BookRight.Web.DependencyInjections
+﻿using BookRight.InfrastructureLib.Persistance.DbContextFiles;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRight.Web.DependencyInjections
 {
     public static class DatabaseDI
     {
+        public static IServiceCollection AddDatabaseDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            var conn = configuration.GetConnectionString("Default");
+            services.AddDbContext<BookRightDbContext>(options => 
+            options.UseSqlServer(conn));
+
+            return services;
+        }
     }
 }
