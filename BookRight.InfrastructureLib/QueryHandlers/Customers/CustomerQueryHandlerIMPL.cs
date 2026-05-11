df8798aa94cfd99c511b5595fd1d6200fcb07b64
Nicolai Helby (nico003t) <nicolaihlb@gmail.com>
2026-05-11 20:55:31 +0200

    Feature-CustomerQuery-Add-GetByIdAsync
    Implemented ICustomerQueries.GetByIdAsync() in CustomerQueryHandlerIMPL.cs

--- BookRight.InfrastructureLib/QueryHandlers/Customers/CustomerQueryHandlerIMPL.cs
+++ BookRight.InfrastructureLib/QueryHandlers/Customers/CustomerQueryHandlerIMPL.cs
@@ -7,13 +7,29 @@ namespace BookRight.InfrastructureLib.QueryHandlers.Customers;
 
 public class CustomerQueryHandlerIMPL(BookRightDbContext db) : ICustomerQueries
 {
-    Task<CustomerDTO?> ICustomerQueries.GetByIdAsync(Guid Id)
+    async Task<CustomerDTO?> ICustomerQueries.GetByIdAsync(Guid Id)
     {
-        throw new NotImplementedException();
+        return await db.Customers
+            .AsNoTracking()
+            .Select(c => new CustomerDTO(
+                c.Id,
+                c.Firstname,
+                c.Lastname,
+                c.Birthdate,
+                c.Note,
+                c.Address.Street,
+                c.Address.PostalCode,
+                c.Address.City,
+                c.Email.EmailAddress,
+                c.PhoneNumber.Number,
+                c.PrefferedTherapist))
+            .FirstOrDefaultAsync();
     }
 
     Task<IReadOnlyList<CustomerDTO>> ICustomerQueries.GetAllAsync()
     {
         throw new NotImplementedException();
     }
+
+    // TODO: Consider also adding the name of PrefferedTherapist to CustomerDTO and finish the implementation.
 }
