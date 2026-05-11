7972f51842c531b17a1eaaabe6a1ac99f20c000d
Nicolai Helby (nico003t) <nicolaihlb@gmail.com>
2026-05-11 20:04:06 +0200

    Feature-CustomerQuery-Add-CustomerQueryHandlerIMPL
    Added CustomerQueryHandlerIMPL.cs with implementation of ICustomerQueries to BookRight.InfrastructureLib

---
+++ BookRight.InfrastructureLib/QueryHandlers/Customers/CustomerQueryHandlerIMPL.cs
@@ -0,0 +1,19 @@
+﻿using BookRight.FacadeLib.DTO;
+using BookRight.FacadeLib.Queries.Customers;
+using BookRight.InfrastructureLib.Persistance.DbContextFiles;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRight.InfrastructureLib.QueryHandlers.Customers;
+
+public class CustomerQueryHandlerIMPL(BookRightDbContext db) : ICustomerQueries
+{
+    Task<CustomerDTO?> ICustomerQueries.GetByIdAsync(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<IReadOnlyList<CustomerDTO>> ICustomerQueries.GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
