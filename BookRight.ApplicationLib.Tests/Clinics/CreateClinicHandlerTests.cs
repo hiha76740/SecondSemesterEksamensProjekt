a783bc9065170389df19febf2b5199b02b5be2b7
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-05-11 08:27:36 +0200

    feature-CreateClinicHandlerTests-Added-Implementation-For-New-OpeningHour-Entity

--- BookRight.ApplicationLib.Tests/Clinics/CreateClinicHandlerTests.cs
+++ BookRight.ApplicationLib.Tests/Clinics/CreateClinicHandlerTests.cs
@@ -1,19 +1,65 @@
 ﻿using BookRight.ApplicationLib.Handlers.Clinics;
 using BookRight.ApplicationLib.Repositories;
 using BookRight.DomainLib.Entities.Clinics;
+using BookRight.DomainLib.Enums;
 using BookRight.FacadeLib.Commands.Clinics.DTOs;
 using BookRight.FacadeLib.Commands.Clinics.Interfaces;
+using BookRight.FacadeLib.DTO;
 using Moq;
 
 namespace BookRight.ApplicationLib.Tests.Clinics;
 
 public class CreateClinicHandlerTests
 {
-    /*
+
+    private static List<OpeningHourDTO> OpeningHoursDTO = new()
+    {
+        new OpeningHourDTO(
+                "Monday",
+                new TimeOnly(8, 0, 0),
+                new TimeOnly(16, 0, 0),
+                false),
+
+        new OpeningHourDTO(
+                "Tuesday",
+                new TimeOnly(8, 0, 0),
+                new TimeOnly(16, 0, 0),
+                false),
+
+        new OpeningHourDTO(
+                "Wednesday",
+                new TimeOnly(8, 0, 0),
+                new TimeOnly(16, 0, 0),
+                false),
+
+        new OpeningHourDTO(
+                "Thursday",
+                new TimeOnly(8, 0, 0),
+                new TimeOnly(16, 0, 0),
+                false),
+
+        new OpeningHourDTO(
+                "Friday",
+                new TimeOnly(8, 0, 0),
+                new TimeOnly(16, 0, 0),
+                false),
+
+        new OpeningHourDTO(
+                "Saturday",
+                new TimeOnly(8, 0, 0),
+                new TimeOnly(16, 0, 0),
+                false),
+
+        new OpeningHourDTO(
+                "Sunday",
+                new TimeOnly(8, 0, 0),
+                new TimeOnly(16, 0, 0),
+                false)
+
+    };
+
     private static CreateClinicCommand CreateCommand()
     {
-        DateTime open = new DateTime(2030, 5, 1, 8, 0, 0);
-        DateTime close = open.AddHours(8);
 
         return new CreateClinicCommand(
             "Ny Klinik Vejle",
@@ -21,8 +67,7 @@ public class CreateClinicHandlerTests
             "7100",
             "Vejle",
             5,
-            open,
-            close);
+            OpeningHoursDTO);
     }
 
     [Fact]
@@ -38,6 +83,5 @@ public class CreateClinicHandlerTests
         mockClinicRepo.Verify(r => r.AddAsync(It.IsAny<Clinic>()), Times.Once);
         mockClinicRepo.Verify(r => r.SaveAsync(), Times.Once);
     }
-    */
 
 }
