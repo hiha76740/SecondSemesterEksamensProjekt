f8633729f6a44781edb6f13c49c004e6cc722514
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-11 14:43:36 +0200

    feature-DiscountStrategyDI-Added

--- BookRight.Web/DependencyInjections/DiscountStrategyDI.cs
+++ BookRight.Web/DependencyInjections/DiscountStrategyDI.cs
@@ -1,6 +1,18 @@
-﻿namespace BookRight.Web.DependencyInjections
+﻿using BookRight.DomainLib.Discounts;
+using BookRight.DomainLib.Discounts.DiscountStrategies;
+
+namespace BookRight.Web.DependencyInjections
 {
     public static class DiscountStrategyDI
     {
+        public static IServiceCollection AddDiscountStrategyDI(this IServiceCollection services)
+        {
+            services.AddTransient<IDiscountStrategy, BronzeLoyalityDiscountStrategy>();
+            services.AddTransient<IDiscountStrategy, SilverLoyalityDiscountStrategy>();
+            services.AddTransient<IDiscountStrategy, GoldLoyalityDiscountStrategy>();
+            services.AddTransient<IDiscountStrategy, BirthdateDiscountStrategy>();
+
+            return services;
+        }
     }
 }
