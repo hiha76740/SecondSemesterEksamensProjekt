1365a5f7cd2cf1581c4d921adaa61926457dc2bb
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-11 07:19:33 +0200

    feature-CreateClinicHandler-Changed-To-Add-Support-For-New-OpeningHour-Entity

--- BookRight.ApplicationLib/Handlers/Clinics/CreateClinicHandler.cs
+++ BookRight.ApplicationLib/Handlers/Clinics/CreateClinicHandler.cs
@@ -1,5 +1,7 @@
 ﻿using BookRight.ApplicationLib.Repositories;
 using BookRight.DomainLib.Entities.Clinics;
+using BookRight.DomainLib.Enums;
+using BookRight.DomainLib.Exceptions;
 using BookRight.DomainLib.ValueObjects;
 using BookRight.FacadeLib.Commands.Clinics.DTOs;
 using BookRight.FacadeLib.Commands.Clinics.Interfaces;
@@ -11,9 +13,23 @@ public class CreateClinicHandler(IClinicRepository clinicRepository) : ICreateCl
     async Task ICreateClinicHandler.Handle(CreateClinicCommand command)
     {
         var address = new Address(command.Street, command.PostalCode, command.City);
-        var openingHours = new OpeningHours(command.OpeningHour, command.CloseingHour);
 
-        var clinic = Clinic.Create(command.Name, command.TreatmentRoomLimit, openingHours, address);
+        List<OpeningHourInput> openingHourInputs = new();
+
+        foreach (var openingHour in command.OpeningHours)
+        {
+            bool exsists = Enum.TryParse<Weekdays>(openingHour.Weekday, out var weekday);
+
+            if (exsists == false)
+                throw new NotFoundException($"Weekday {openingHour.Weekday} was not found");
+
+            var openingHourInput = new OpeningHourInput(weekday, openingHour.OpeningTime, openingHour.ClosingTime, openingHour.IsClosed);
+
+            openingHourInputs.Add(openingHourInput);
+        }
+
+
+        var clinic = Clinic.Create(command.Name, command.TreatmentRoomLimit, openingHourInputs, address);
 
         await clinicRepository.AddAsync(clinic);
         await clinicRepository.SaveAsync();
