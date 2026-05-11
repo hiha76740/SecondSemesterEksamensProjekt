561f1fafc6bee32d3e2f2c53a44fb11dcdc1ce47
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-05-11 12:17:03 +0200

    feature-Added-PriceCalculatorService

---
+++ BookRight.ApplicationLib/Services/PriceCalculatorService.cs
@@ -0,0 +1,82 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Discounts;
+using BookRight.DomainLib.Enums;
+using BookRight.DomainLib.Exceptions;
+using BookRight.FacadeLib.Commands.Pricing.DTOs;
+using BookRight.FacadeLib.Commands.Pricing.Interfaces;
+using BookRight.FacadeLib.DTO;
+
+namespace BookRight.ApplicationLib.Services
+{
+    public class PriceCalculatorService : IPriceCalculatorService
+    {
+        private readonly IEnumerable<IDiscountStrategy> _strategies;
+        private readonly IBookingRepository _bookingRepository;
+        private readonly ICustomerRepository _CustomerRepository;
+        private readonly ITreatmentRepository _treatmentRepository;
+
+        public PriceCalculatorService(
+            IEnumerable<IDiscountStrategy> strategies,
+            ICustomerRepository CustomerRepository,
+            ITreatmentRepository treatmentRepository,
+            IBookingRepository bookingRepository)
+        {
+            ArgumentNullException.ThrowIfNull(strategies);
+            ArgumentNullException.ThrowIfNull(CustomerRepository);
+            ArgumentNullException.ThrowIfNull(treatmentRepository);
+            ArgumentNullException.ThrowIfNull(bookingRepository);
+
+            _strategies = strategies;
+            _CustomerRepository = CustomerRepository;
+            _treatmentRepository = treatmentRepository;
+            _bookingRepository = bookingRepository;
+
+            if (_strategies.Any() == false)
+                throw new ArgumentException(
+                    "There has to be atleast 1 strategy.", nameof(strategies));
+
+        }
+
+
+        async Task<PriceCalculatorDTO> IPriceCalculatorService.CalculatePrice(PriceCalculatorCommand command)
+        {
+            Lock _lock = new Lock();
+
+            ArgumentNullException.ThrowIfNull(command);
+
+            var Customer = await _CustomerRepository.GetByIdAsync(command.CustomerId)
+            ?? throw new NotFoundException("Customer not found");
+
+            var CustomerHistorySum = await _bookingRepository.GetBookingHistorySumAsync(command.CustomerId, 12);
+
+            var treatment = await _treatmentRepository.GetByIdAsync(command.TreatmentId)
+            ?? throw new NotFoundException("Treatment not found");
+
+            var numberOfBirthdayDiscountUsed = await _bookingRepository.GetNumberOfBirthdayDiscountThisYearById(command.CustomerId, command.BookingDate.Year);
+
+            var PriceCalculatorDTO = new PriceCalculatorInput(
+                treatment.Price,
+                command.BookingDate,
+                Customer.Birthdate,
+                CustomerHistorySum,
+                numberOfBirthdayDiscountUsed);
+
+            PriceCalculatorResult bestDiscount = new PriceCalculatorResult(treatment.Price, 0, DiscountTypes.None);
+
+            Parallel.ForEach(_strategies, s =>
+                {
+                    var discountResult = s.CalculatePrice(PriceCalculatorDTO);
+
+                    lock (_lock)
+                    {
+                        if (discountResult.FinalPrice > bestDiscount.FinalPrice)
+                        {
+                            bestDiscount = discountResult;
+                        }
+                    }    
+                });
+
+            return new PriceCalculatorDTO(bestDiscount.NormalPrice, bestDiscount.FinalPrice, bestDiscount.DiscountType.ToString());
+        }
+    }
+}
