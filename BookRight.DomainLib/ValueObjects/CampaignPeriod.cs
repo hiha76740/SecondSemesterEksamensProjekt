c05a8949e78a11f10bc9708c17ca0ad81d53f616
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:00:47 +0200

    feature-Campaign-Added-CampaignPeriod-Value-Object

---
+++ BookRight.DomainLib/ValueObjects/CampaignPeriod.cs
@@ -0,0 +1,18 @@
+﻿using BookRight.DomainLib.Exceptions;
+
+namespace BookRight.DomainLib.ValueObjects;
+
+public record CampaignPeriod
+{
+    public DateTime From { get; init; }
+    public DateTime To { get; init; }
+
+    public CampaignPeriod(DateTime from, DateTime to)
+    {
+        if (from >= to)
+            throw new DomainException("End can not be before or Equal to start");
+
+        To = to;
+        From = from;
+    }
+}
