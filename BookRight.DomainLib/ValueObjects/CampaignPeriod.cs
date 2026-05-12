2226d662cbec33b0442427155378b197a285a0de
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:09:13 +0200

    feature-CampaignPeriod-Change-To-DateOnly
    Changed from DateTime to DateOnly

--- BookRight.DomainLib/ValueObjects/CampaignPeriod.cs
+++ BookRight.DomainLib/ValueObjects/CampaignPeriod.cs
@@ -4,10 +4,10 @@ namespace BookRight.DomainLib.ValueObjects;
 
 public record CampaignPeriod
 {
-    public DateTime From { get; init; }
-    public DateTime To { get; init; }
+    public DateOnly From { get; init; }
+    public DateOnly To { get; init; }
 
-    public CampaignPeriod(DateTime from, DateTime to)
+    public CampaignPeriod(DateOnly from, DateOnly to)
     {
         if (from >= to)
             throw new DomainException("End can not be before or Equal to start");
