1aa5453cd81d2db984aabe2d19b42e47bc98352a
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 04:59:53 +0200

    feature-Campaign-Added-Campaign-AggregateRoot
    Added with Create Factory method

---
+++ BookRight.DomainLib/Entities/Campaigns/Campaign.cs
@@ -0,0 +1,39 @@
+﻿using BookRight.DomainLib.Enums;
+using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+
+namespace BookRight.DomainLib.Entities.Campaigns;
+
+public class Campaign : AggregateRoot
+{
+    public string Name { get; init; }
+    public decimal DiscountProcentage { get; init; }
+    public CampaignPeriod CampaignPeriod { get; init; }
+    public Guid TreatmentId { get; init; }
+    public CampaignStatus Status { get; private set; }
+
+
+    public static Campaign Create(string name, decimal discountProcentage, CampaignPeriod campaignPeriod, Guid treatmentId)
+    {
+        return new Campaign(name, discountProcentage, campaignPeriod, treatmentId);
+    }
+
+    private Campaign(string name, decimal discountProcentage, CampaignPeriod campaignPeriod, Guid treatmentId)
+    {
+        if (campaignPeriod.From <= DateTime.Today)
+            throw new DomainException("Start date must be after todays date");
+
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainException("Campain must have a name");
+
+        if (discountProcentage <= 0)
+            throw new DomainException("Discount procentage can not be 0 or less");
+
+
+        Name = name;
+        DiscountProcentage = discountProcentage;
+        CampaignPeriod = campaignPeriod;
+        TreatmentId = treatmentId;
+        Status = CampaignStatus.Active;
+    }
+}
