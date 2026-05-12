6c308893cbc5c3ffd4a35e1f5a3c8d606105a41d
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:15:42 +0200

    feature-Campaign-Added-Handler-Implementation-And-Repository-Interface

---
+++ BookRight.ApplicationLib/Handlers/Campaigns/CreateCampaignHandler.cs
@@ -0,0 +1,24 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Campaigns;
+using BookRight.DomainLib.Exceptions;
+using BookRight.DomainLib.ValueObjects;
+using BookRight.FacadeLib.Commands.Campaigns.DTOs;
+using BookRight.FacadeLib.Commands.Campaigns.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Campaigns;
+
+public class CreateCampaignHandler(ICampaignRepository campaignRepository, ITreatmentRepository treatmentRepository) : ICreateCampaignHandler
+{
+    async Task ICreateCampaignHandler.Handle(CreateCampaignCommand command)
+    {
+        _ = await treatmentRepository.GetByIdAsync(command.TreatmentId)
+           ?? throw new NotFoundException("Treatment not found");
+
+        var campaignPeriod = new CampaignPeriod(command.StartDate, command.EndDate);
+
+        var campaign = Campaign.Create(command.Name, command.DiscountProcentage, campaignPeriod, command.TreatmentId);
+
+        await campaignRepository.AddAsync(campaign);
+        await campaignRepository.SaveAsync();
+    }
+}
---
+++ BookRight.ApplicationLib/Repositories/ICampaignRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookRight.DomainLib.Entities.Campaigns;
+
+namespace BookRight.ApplicationLib.Repositories
+{
+    public interface ICampaignRepository
+    {
+        Task AddAsync(Campaign campaign);
+        Task SaveAsync();
+    }
+}
