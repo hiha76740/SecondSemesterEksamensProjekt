36207ac6fb96d75945d11c6289aeca8c7216014f
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:22:17 +0200

    feature-Campaign-Added-CampaignQueries

---
+++ BookRight.FacadeLib/Queries/Campaigns/ICampaignQueries.cs
@@ -0,0 +1,9 @@
+﻿using BookRight.FacadeLib.DTO;
+
+namespace BookRight.FacadeLib.Queries.Campaigns;
+
+public interface ICampaignQueries
+{
+    Task<CampaignDTO?> GetByIdAsync(Guid Id);
+    Task<IReadOnlyList<CampaignDTO>> GetAllAsync();
+}
