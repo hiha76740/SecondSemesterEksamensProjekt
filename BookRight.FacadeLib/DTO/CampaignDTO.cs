ee2c56c09e5c7c926b4ef2ac9ffcd283193891bf
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:24:34 +0200

    feature-Campaign-Added-CampaignDTO-For-Queries

---
+++ BookRight.FacadeLib/DTO/CampaignDTO.cs
@@ -0,0 +1,3 @@
+﻿namespace BookRight.FacadeLib.DTO;
+
+public record CampaignDTO(string Name,decimal DiscountProcentage,DateOnly CampaignStart, DateOnly CampaignEnd, Guid TreatmentId, string Status);
