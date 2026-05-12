651c8d152a63d158ab651b67096acb48f5adf853
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:15:15 +0200

    feature-Campaign-Added-Interface-For-Handler-And-Command

---
+++ BookRight.FacadeLib/Commands/Campaigns/DTOs/CreateCampaignCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace BookRight.FacadeLib.Commands.Campaigns.DTOs;
+
+public record CreateCampaignCommand(string Name, int DiscountProcentage, DateOnly StartDate, DateOnly EndDate, Guid TreatmentId);
+
