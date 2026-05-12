b835ca5fa148def1c45976c0a744bd90d505b0ea
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 12:09:23 +0200

    feature-Added-Missing-List-properties-And-Maps-them-as-Json

--- BookRight.InfrastructureLib/Persistance/EF Configurations/BookingConfiguration.cs
+++ BookRight.InfrastructureLib/Persistance/EF Configurations/BookingConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using BookRight.DomainLib.Entities.Bookings;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.Json;
 
 namespace BookRight.InfrastructureLib.Persistance.EF_Configurations;
 
@@ -17,5 +18,15 @@ internal class BookingConfiguration : IEntityTypeConfiguration<Booking>
         builder.ComplexProperty(
             b => b.Time,
             t => t.ToJson());
+
+        builder.Property<List<Guid>>("_participants")
+            .HasColumnName("Participants")
+            .HasConversion(
+            v => JsonSerializer.Serialize(
+                v.Select(x => x).ToList(),
+                (JsonSerializerOptions?)null),
+
+            v => JsonSerializer.Deserialize<List<Guid>>(v, (JsonSerializerOptions?)null)!
+            .ToList());
     }
 }
