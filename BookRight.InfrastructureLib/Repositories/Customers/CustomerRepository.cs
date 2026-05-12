7ba2af3e69041a05a3a6d7147de342671bc4d3f0
Nicolai Helby (nico003t) <nicolaihlb@gmail.com>
2026-05-12 10:01:52 +0200

    Feature-CustomerRepository-Add
    Added CustomerRepository.cs with explicit implementation of ICustomerRepository to BookRight.InfrastructureLib and removed Dummy.cs in same directory

---
+++ BookRight.InfrastructureLib/Repositories/Customers/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Entities.Customers;
+
+namespace BookRight.InfrastructureLib.Repositories.Customers;
+
+public class CustomerRepository(BookRightDbContext db) : ICustomerRepository
+{
+    Task<Customer?> ICustomerRepository.GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task ICustomerRepository.AddAsync(Customer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task ICustomerRepository.SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
