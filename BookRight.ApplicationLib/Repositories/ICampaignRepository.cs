b0a2409ce825f523330efb1fdc323282523ab6a1
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:40:32 +0200

    feature-ICampaignRepository-Added-GetActiveCampaigns-Method

--- BookRight.ApplicationLib/Repositories/ICampaignRepository.cs
+++ BookRight.ApplicationLib/Repositories/ICampaignRepository.cs
@@ -5,6 +5,7 @@ namespace BookRight.ApplicationLib.Repositories
     public interface ICampaignRepository
     {
         Task AddAsync(Campaign campaign);
+        Task<IReadOnlyList<Campaign>> GetActiveCampaignsAsync();
         Task<Campaign?> GetByIdAsync(Guid campaignId);
         Task SaveAsync();
     }
