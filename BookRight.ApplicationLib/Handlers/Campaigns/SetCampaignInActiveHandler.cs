ff8daf41abe16f88396cf065f40cc50c25896f6c
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:19:15 +0200

    feature-Campaign-Added-SetCampaignInActiveHandler

---
+++ BookRight.ApplicationLib/Handlers/Campaigns/SetCampaignInActiveHandler.cs
@@ -0,0 +1,19 @@
+﻿using BookRight.ApplicationLib.Repositories;
+using BookRight.DomainLib.Exceptions;
+using BookRight.FacadeLib.Commands.Campaigns.DTOs;
+using BookRight.FacadeLib.Commands.Campaigns.Interfaces;
+
+namespace BookRight.ApplicationLib.Handlers.Campaigns;
+
+public class SetCampaignInActiveHandler(ICampaignRepository campaignRepository) : ISetCampaignInActiveHandler
+{
+    async Task ISetCampaignInActiveHandler.Handle(SetCampaignInActiveCommand command)
+    {
+        var campaign = await campaignRepository.GetByIdAsync(command.CampaignId)
+            ?? throw new NotFoundException("Campaign not found");
+
+        campaign.SetInaktive();
+
+        await campaignRepository.SaveAsync();
+    }
+}
