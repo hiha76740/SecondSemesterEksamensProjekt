cdd6e1ecc46ff2cf67422534f3f721d5e14f6096
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 05:17:31 +0200

    feature-Campaign-Added-Interface-For-Handler-And-DTO-For-SetCampaignInActive

---
+++ BookRight.FacadeLib/Commands/Campaigns/Interfaces/ISetCampaignInActiveHandler.cs
@@ -0,0 +1,8 @@
+﻿using BookRight.FacadeLib.Commands.Campaigns.DTOs;
+
+namespace BookRight.FacadeLib.Commands.Campaigns.Interfaces;
+
+public interface ISetCampaignInActiveHandler
+{
+    Task Handle(SetCampaignInActiveCommand command);
+}
