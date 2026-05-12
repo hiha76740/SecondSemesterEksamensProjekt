aa175adc3debfeb0e98693cd7894c63937795292
Nicolai Helby (nico003t) <nicolaihlb@gmail.com>
2026-05-12 16:43:10 +0200

    Feature-CustomerRepository-Add-SaveAsync
    Implemented ICustomerRepository.SaveAsync() in CustomerRepository.cs

--- BookRight.InfrastructureLib/Repositories/Customers/CustomerRepository.cs
+++ BookRight.InfrastructureLib/Repositories/Customers/CustomerRepository.cs
@@ -1,21 +1,23 @@
 ﻿using BookRight.ApplicationLib.Repositories;
 using BookRight.DomainLib.Entities.Customers;
+using BookRight.InfrastructureLib.Persistance.DbContextFiles;
 
 namespace BookRight.InfrastructureLib.Repositories.Customers;
 
 public class CustomerRepository(BookRightDbContext db) : ICustomerRepository
 {
     async Task<Customer?> ICustomerRepository.GetByIdAsync(Guid id)
-    {
+    { 
        return await db.Customers.FindAsync(id); 
     }
+
     async Task ICustomerRepository.AddAsync(Customer customer)
     {
         await db.Customers.AddAsync(customer);
     }
 
-    Task ICustomerRepository.SaveAsync()
+    async Task ICustomerRepository.SaveAsync()
     {
-        throw new NotImplementedException();
+        await db.SaveChangesAsync();
     }
 }
