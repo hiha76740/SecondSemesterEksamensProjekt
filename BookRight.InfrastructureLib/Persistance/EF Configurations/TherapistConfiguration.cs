0357d1922952102c8357e98269f7100595915829
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-12 10:41:12 +0200

    feature-TherapistConfiguration-Added-Setup-For-ValueObject-And-Enum

--- BookRight.InfrastructureLib/Persistance/EF Configurations/TherapistConfiguration.cs
+++ BookRight.InfrastructureLib/Persistance/EF Configurations/TherapistConfiguration.cs
@@ -8,6 +8,19 @@ internal class TherapistConfiguration : IEntityTypeConfiguration<Therapist>
 {
     public void Configure(EntityTypeBuilder<Therapist> builder)
     {
-        throw new NotImplementedException();
+        builder.Property(t => t.CertificationTypes)
+           .HasConversion<string>();
+
+        builder.ComplexProperty(
+            t => t.Address,
+            a => a.ToJson());
+
+        builder.ComplexProperty(
+            t => t.Email,
+            e => e.ToJson());
+
+        builder.ComplexProperty(
+            t => t.PhoneNumber,
+            p => p.ToJson());
     }
 }
