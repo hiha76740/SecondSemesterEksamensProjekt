43611e6eca5fc07f448e814b5ed2458bc92ba7a9
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-17 07:41:38 +0200

    feature-Added-CertificationTypeQuery-Implementation

---
+++ BookRight.InfrastructureLib/QueryHandlers/CertificationType/CertificationTypeQueryHandlerIMPL.cs
@@ -0,0 +1,14 @@
+﻿using BookRight.DomainLib.Enums;
+using BookRight.FacadeLib.DTO;
+using BookRight.FacadeLib.Queries.CertificationTypes;
+
+namespace BookRight.InfrastructureLib.QueryHandlers.CertificationType;
+
+public class CertificationTypeQueryHandlerIMPL : ICertificationTypeQueries
+{
+    IReadOnlyList<CertificationTypeDTO> ICertificationTypeQueries.GetAll()
+    {
+        return Enum.GetValues<CertificationTypes>()
+                .Select(ct => new CertificationTypeDTO(ct.ToString())).ToList();
+    }
+}
