1b552cb4de21cb12b1e5148a73bc3c11159139d7
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-17 07:47:28 +0200

    feature-Added-missing-services

--- BookRight.Web/DependencyInjections/HandlerDI.cs
+++ BookRight.Web/DependencyInjections/HandlerDI.cs
@@ -1,7 +1,15 @@
 ﻿using BookRight.ApplicationLib.Handlers.Bookings;
+using BookRight.ApplicationLib.Handlers.Campaigns;
+using BookRight.ApplicationLib.Handlers.Clinics;
+using BookRight.ApplicationLib.Handlers.Customers;
+using BookRight.ApplicationLib.Handlers.Therapists;
 using BookRight.ApplicationLib.Services;
 using BookRight.FacadeLib.Commands.Booking.Interfaces;
+using BookRight.FacadeLib.Commands.Campaigns.Interfaces;
+using BookRight.FacadeLib.Commands.Clinics.Interfaces;
+using BookRight.FacadeLib.Commands.Customers.Interfaces;
 using BookRight.FacadeLib.Commands.Pricing.Interfaces;
+using BookRight.FacadeLib.Commands.Therapists.Interfaces;
 
 namespace BookRight.Web.DependencyInjections
 {
@@ -9,6 +17,7 @@ namespace BookRight.Web.DependencyInjections
     {
         public static IServiceCollection AddHandlerDI(this IServiceCollection services)
         {
+            // Booking
             services.AddScoped<ICreateBookingHandler, CreateBookingHandler>();
             services.AddScoped<ICancelBookingHandler, CancelBookingHandler>();
             services.AddScoped<IChangeClinicHandler, ChangeClinicHandler>();
@@ -21,7 +30,23 @@ namespace BookRight.Web.DependencyInjections
             services.AddScoped<IAddParticipantHandler, AddParticipantHandler>();
             services.AddScoped<IRemoveParticipantHandler, RemoveParticipantHandler>();
 
+            // Price Calculator
             services.AddScoped<IPriceCalculatorService, PriceCalculatorService>();
+
+            // Customer
+            services.AddScoped<ICreateCustomerHandler, CreateCustomerHandler>();
+            services.AddScoped<IChangeCustomerInfoHandler, ChangeCustomerInfoHandler>();
+
+            // Therapist
+            services.AddScoped<ICreateTherapistHandler, CreateTherapistHandler>();
+            services.AddScoped<IChangeTherapistInfoHandler, ChangeTherapistInfoHandler>();
+
+            // Clinic
+            services.AddScoped<ICreateClinicHandler, CreateClinicHandler>();
+            services.AddScoped<IChangeClinicInfoHandler, ChangeClinicInfoHandler>();
+
+            // Campaign
+            services.AddScoped<ICreateCampaignHandler, CreateCampaignHandler>();
 
             return services;
         }
