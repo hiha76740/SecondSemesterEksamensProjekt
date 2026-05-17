5d027bbd60b7c3e6bdddc4016c1fe3c67971f00e
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-17 07:42:51 +0200

    feature-Added-TherapistQuery-Implementation

---
+++ BookRight.InfrastructureLib/QueryHandlers/Therapists/TherapistQueryHandlerIMPL.cs
@@ -0,0 +1,55 @@
+﻿using BookRight.FacadeLib.DTO;
+using BookRight.FacadeLib.Queries.Therapists;
+using BookRight.InfrastructureLib.Persistance.DbContextFiles;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRight.InfrastructureLib.QueryHandlers.Therapists;
+
+public class TherapistQueryHandlerIMPL(BookRightDbContext db) : ITherapistQueries
+{
+    async Task<IReadOnlyList<TherapistDTO>> ITherapistQueries.GetAllAsync()
+    {
+        var therapists = await db.Therapists
+       .AsNoTracking()
+       .ToListAsync();
+
+        return therapists
+        .Select(t => new TherapistDTO(
+            t.Id,
+            t.AuthorizationNumber,
+            t.Name,
+            t.HourlyRate,
+            t.Address.Street,
+            t.Address.PostalCode,
+            t.Address.City,
+            t.Email.EmailAddress,
+            t.PhoneNumber.Number,
+            t.CertificationTypes.Select(ct => ct.ToString()).ToList(),
+            t.AssociatedClinics.ToList()
+        ))
+        .ToList();
+    }
+
+    async Task<TherapistDTO?> ITherapistQueries.GetByIdAsync(Guid Id)
+    {
+        var therapist = await db.Therapists
+            .AsNoTracking()
+            .Where(t => t.Id == Id)
+            .ToListAsync();
+
+        return therapist.Select(t => new TherapistDTO(
+            t.Id,
+            t.AuthorizationNumber,
+            t.Name,
+            t.HourlyRate,
+            t.Address.Street,
+            t.Address.PostalCode,
+            t.Address.City,
+            t.Email.EmailAddress,
+            t.PhoneNumber.Number,
+            t.CertificationTypes.Select(ct => ct.ToString()).ToList(),
+            t.AssociatedClinics.ToList()
+            ))
+        .FirstOrDefault();
+    }
+}
