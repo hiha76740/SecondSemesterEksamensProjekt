8947a8232e660f2dc9582a543b8f6eeb64f6df50
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-17 07:36:35 +0200

    feature-Added-TherapistDTO

---
+++ BookRight.FacadeLib/DTO/TherapistDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace BookRight.FacadeLib.DTO;
+
+public record TherapistDTO(
+    Guid Id,
+    string AuthorizationNumber,
+    string Name,
+    decimal HourlyRate,
+    string Street,
+    string PostalCode,
+    string City,
+    string Email,
+    string PhoneNumber,
+    IReadOnlyList<string> CertificationTypes,
+    IReadOnlyList<Guid> AssociatedClinics
+    );
