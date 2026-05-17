9e2542f303235b8dcc02606e852fa5cbfef78209
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-17 07:38:51 +0200

    feature-Added-Interface-For-CertificationType-Queries

---
+++ BookRight.FacadeLib/Queries/CertificationTypes/ICertificationTypeQueries.cs
@@ -0,0 +1,8 @@
+﻿using BookRight.FacadeLib.DTO;
+
+namespace BookRight.FacadeLib.Queries.CertificationTypes;
+
+public interface ICertificationTypeQueries
+{
+    IReadOnlyList<CertificationTypeDTO> GetAll();
+}
