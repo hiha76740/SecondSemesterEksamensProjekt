f32d64d02bd6e5a56a474a88484700cc318956d1
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-17 07:47:11 +0200

    feature-Added-AddDomainDI-Static-Method

--- BookRight.Web/DependencyInjections/DomainDI.cs
+++ BookRight.Web/DependencyInjections/DomainDI.cs
@@ -1,6 +1,15 @@
-﻿namespace BookRight.Web.DependencyInjections
+﻿using BookRight.DomainLib.Services;
+
+namespace BookRight.Web.DependencyInjections
 {
     public static class DomainDI
     {
+        public static IServiceCollection AddDomainDI(this IServiceCollection services)
+        {
+            services.AddScoped<IBookingCapacityService, BookingCapacityService>();
+            services.AddScoped<IValidateOverlapService, ValidateOverlapService>();
+
+            return services;
+        }
     }
 }
