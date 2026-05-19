bb0d2e317f82541f7f22d088fa7a0775fdf160fd
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-19 08:18:32 +0200

    feature-ClinicQueryHandlerIMPL-Added-OrderBy-Weekday
    converted to in-memory DTO mapping after sorting by Weekday to be able to get weekdays in correct order

--- BookRight.InfrastructureLib/QueryHandlers/Clinics/ClinicQueryHandlerIMPL.cs
+++ BookRight.InfrastructureLib/QueryHandlers/Clinics/ClinicQueryHandlerIMPL.cs
@@ -9,32 +9,44 @@ public class ClinicQueryHandlerIMPL(BookRightDbContext db) : IClinicQueries
 {
     async Task<IReadOnlyList<ClinicDTO>> IClinicQueries.GetAllAsync()
     {
-        return await db.Clinics
+        var clinics = await db.Clinics
             .AsNoTracking()
-            .Select(c => new ClinicDTO(
-            c.Id,
-            c.Name,
-            c.TreatmentRoomLimit,
-            c.OpeningHours
-                .Select(oh => new OpeningHourDTO(oh.WeekDay.ToString(),oh.OpeningTime,oh.ClosingTime,oh.IsClosed)).ToList(),
-            c.Address.Street,
-            c.Address.PostalCode,
-            c.Address.City)).ToListAsync();
+            .Include(oh => oh.OpeningHours)
+            .ToListAsync();
+
+
+        return clinics.Select(c => new ClinicDTO(
+         c.Id,
+         c.Name,
+         c.TreatmentRoomLimit,
+         c.OpeningHours
+         .OrderBy(oh => oh.WeekDay)
+         .Select(oh => new OpeningHourDTO(oh.WeekDay.ToString(), oh.OpeningTime, oh.ClosingTime, oh.IsClosed)).ToList(),
+         c.Address.Street,
+         c.Address.PostalCode,
+         c.Address.City))
+         .ToList();
     }
 
     async Task<ClinicDTO?> IClinicQueries.GetByIdAsync(Guid Id)
     {
-        return await db.Clinics
+        var clinic = await db.Clinics
             .AsNoTracking()
+            .Include(oh => oh.OpeningHours)
             .Where(c => c.Id == Id)
-            .Select(c => new ClinicDTO(
-                c.Id,
-                c.Name,
-                c.TreatmentRoomLimit,
-                c.OpeningHours
-                .Select(oh => new OpeningHourDTO(oh.WeekDay.ToString(), oh.OpeningTime, oh.ClosingTime, oh.IsClosed)).ToList(),
-                c.Address.Street,
-                c.Address.PostalCode,
-                c.Address.City)).FirstOrDefaultAsync();
+            .ToListAsync();
+
+        return clinic.Select(c => new ClinicDTO(
+          c.Id,
+          c.Name,
+          c.TreatmentRoomLimit,
+          c.OpeningHours
+          .OrderBy(oh => oh.WeekDay)
+          .Select(oh => new OpeningHourDTO(oh.WeekDay.ToString(), oh.OpeningTime, oh.ClosingTime, oh.IsClosed))
+          .ToList(),
+          c.Address.Street,
+          c.Address.PostalCode,
+          c.Address.City))
+           .FirstOrDefault();
     }
 }
