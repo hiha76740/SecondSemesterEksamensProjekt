e2952e19364dc7d98efa3fad1c6407d79ebac824
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-22 05:42:56 +0200

    refactor-Changed-namespace-to-standard

--- BookRight.Web/DependencyInjections/DiscountStrategyDI.cs
+++ BookRight.Web/DependencyInjections/DiscountStrategyDI.cs
@@ -1,20 +1,19 @@
 ﻿using BookRight.DomainLib.Discounts;
 using BookRight.DomainLib.Discounts.DiscountStrategies;
 
-namespace BookRight.Web.DependencyInjections
-{
-    public static class DiscountStrategyDI
-    {
-        public static IServiceCollection AddDiscountStrategyDI(this IServiceCollection services)
-        {
-            services.AddTransient<IDiscountStrategy, BronzeLoyalityDiscountStrategy>();
-            services.AddTransient<IDiscountStrategy, SilverLoyalityDiscountStrategy>();
-            services.AddTransient<IDiscountStrategy, GoldLoyalityDiscountStrategy>();
-            services.AddTransient<IDiscountStrategy, BirthDateDiscountStrategy>();
-            services.AddTransient<IDiscountStrategy, CampaignDiscountStrategy>();
-            services.AddTransient<IDiscountStrategy, NoDiscountStrategy>();
+namespace BookRight.Web.DependencyInjections;
 
-            return services;
-        }
+public static class DiscountStrategyDI
+{
+    public static IServiceCollection AddDiscountStrategyDI(this IServiceCollection services)
+    {
+        services.AddTransient<IDiscountStrategy, BronzeLoyalityDiscountStrategy>();
+        services.AddTransient<IDiscountStrategy, SilverLoyalityDiscountStrategy>();
+        services.AddTransient<IDiscountStrategy, GoldLoyalityDiscountStrategy>();
+        services.AddTransient<IDiscountStrategy, BirthDateDiscountStrategy>();
+        services.AddTransient<IDiscountStrategy, CampaignDiscountStrategy>();
+        services.AddTransient<IDiscountStrategy, NoDiscountStrategy>();
+
+        return services;
     }
 }
