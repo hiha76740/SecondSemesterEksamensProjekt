7d29812c87474a3bff6ac66b63a96c1697ba9262
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-22 14:51:25 +0200

    feature-TherapistQueries-Implemented-Search-method

--- BookRight.InfrastructureLib/QueryHandlers/Therapists/TherapistQueryHandlerIMPL.cs
+++ BookRight.InfrastructureLib/QueryHandlers/Therapists/TherapistQueryHandlerIMPL.cs
@@ -2,6 +2,7 @@
 using BookRight.FacadeLib.Queries.Therapists;
 using BookRight.InfrastructureLib.Persistance.DbContextFiles;
 using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
 
 namespace BookRight.InfrastructureLib.QueryHandlers.Therapists;
 
@@ -51,5 +52,33 @@ public class TherapistQueryHandlerIMPL(BookRightDbContext db) : ITherapistQuerie
             t.AssociatedClinics.ToList()
             ))
         .FirstOrDefault();
+    }
+
+    async Task<IReadOnlyList<TherapistDTO>> ITherapistQueries.SearchTherapistAsync(string filter)
+    {
+        var therapist = await db.Therapists
+       .AsNoTracking()
+       .Where(t =>
+       t.Name.Contains(filter) ||
+       t.Email.EmailAddress.Contains(filter) ||
+       t.PhoneNumber.Number.Contains(filter))
+       .ToListAsync();
+
+
+        return therapist
+            .Select(t => new TherapistDTO(
+                t.Id,
+                t.AuthorizationNumber,
+                t.Name,
+                t.HourlyRate,
+                t.Address.Street,
+                t.Address.PostalCode,
+                t.Address.City,
+                t.Email.EmailAddress,
+                t.PhoneNumber.Number,
+                t.CertificationTypes.Select(ct => ct.ToString()).ToList(),
+                t.AssociatedClinics.ToList()
+                ))
+            .ToList();
     }
 }
