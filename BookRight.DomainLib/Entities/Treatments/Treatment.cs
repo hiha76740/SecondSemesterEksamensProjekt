24101329928a0a59d90c36f70154d157dd06f5f4
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-05-27 20:00:16 +0200

    refactor-Treatment-Added-Summary

--- BookRight.DomainLib/Entities/Treatments/Treatment.cs
+++ BookRight.DomainLib/Entities/Treatments/Treatment.cs
@@ -3,6 +3,13 @@ using BookRight.DomainLib.Exceptions;
 
 namespace BookRight.DomainLib.Entities.Treatments;
 
+/// <summary>
+/// Represents a treatment offered by the domain, including its name, price, duration, required certification, and
+/// participant limit.
+/// </summary>
+/// <remarks>Instances are created via Create; an Id is generated automatically. The constructor and
+/// ChangeMaxParticipants validate inputs and throw DomainException for invalid values (name must be non-empty, price
+/// must be at least 1, and max participants must be at least 1).</remarks>
 public class Treatment : AggregateRoot
 {
     public string Name { get; init; }
@@ -11,13 +18,26 @@ public class Treatment : AggregateRoot
     public CertificationTypes CertificationRequired { get; init; }
     public int MaxParticipants { get; private set; }
 
-
+    /// <summary>
+    /// Creates a new Treatment with the specified name, price, duration, required certification, and maximum
+    /// participants.
+    /// </summary>
+    /// <param name="name">Name of the treatment.</param>
+    /// <param name="price">Price of the treatment.</param>
+    /// <param name="duration">Duration of the treatment.</param>
+    /// <param name="certificationTypeRequired">Required certification type to perform the treatment.</param>
+    /// <param name="maxParticipants">Maximum number of participants allowed.</param>
+    /// <returns>A Treatment instance initialized with the provided values.</returns>
     public static Treatment Create(string name, decimal price, TimeSpan duration, CertificationTypes certificationTypeRequired, int maxParticipants)
     {
         return new Treatment(name, price, duration, certificationTypeRequired, maxParticipants);
     }
 
-
+    /// <summary>
+    /// Sets the maximum number of participants for the entity.
+    /// </summary>
+    /// <param name="newMaxParticipantsAmount">The new maximum number of participants. Must be at least 1.</param>
+    /// <exception cref="DomainException">Thrown when newMaxParticipantsAmount is less than 1.</exception>
     public void ChangeMaxParticipants(int newMaxParticipantsAmount)
     {
         if (newMaxParticipantsAmount < 1)
@@ -25,7 +45,6 @@ public class Treatment : AggregateRoot
 
         MaxParticipants = newMaxParticipantsAmount;
     }
-
 
     private Treatment(string name, decimal price, TimeSpan duration, CertificationTypes certificationRequired, int maxParticipants)
     {
